f4588b1b69a8883f4468c2c90b8f2cf6d765bebc
Ernest Voon <ernestvooncs@outlook.com>
2019-08-29 18:48:51 +0800

    completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        // System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+        String input;
+        System.out.println("\n\t____________________________________________________________" 
+        +"\n\tHello! I'm Duke\n\tWhat can I do for you?"
+        + "\n\t____________________________________________________________");
+
+        do {
+            input = sc.next();
+            switch(input){
+                case "bye":
+                    System.out.println("\n\t____________________________________________________________" + 
+                    "\n\tBye. Hope to see you again soon!" +
+                    "\n\t____________________________________________________________");
+                    break;
+                default:
+                    System.out.println("\n\t____________________________________________________________\n\t" 
+                    + input 
+                    + "\n\t____________________________________________________________");
+            }
+        }
+        while(!(input.equals("bye")));
     }
 }
