7ae2231571abff10adaaca8aef3692898ef81790
wangwenwei <e0373972@u.nus.edu>
2019-08-31 21:26:19 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke" + System.lineSeparator() + "What can I do for you?");
+
+
+        line = in.nextLine();
+        System.out.println(line);
+
+        do{
+            line = in.nextLine();
+            System.out.println(line);
+        }while(!line.equals("bye"));
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
