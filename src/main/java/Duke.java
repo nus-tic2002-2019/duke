094affb91d712f683d5e1d864bb36ac9ff1e4dac
e0260237 <e0260237@u.nus.edu>
2019-08-31 16:55:11 +0800

    Level 1 & level 2 Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +10,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        String inData = null;
+        ArrayList<String> arlist = new ArrayList<String>( );
+        while (!Objects.equals(inData, "bye")){
+            Scanner scan = new Scanner(System.in);
+            inData = scan.nextLine();
+            if(Objects.equals(inData, "list")){
+                System.out.println(arlist);
+            }
+            if(!Objects.equals(inData, "list")){
+                if(!Objects.equals(inData, "bye")) {
+                    System.out.println("added:" + inData);
+                    arlist.add(inData + "\n");
+                }
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
+
+
