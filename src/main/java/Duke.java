b8c480dbab47f8bd686dd7b7528ed14b8c29853d
Ernest Voon <ernestvooncs@outlook.com>
2019-08-31 16:17:02 +0800

    updated with Duke Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
-
 public class Duke {
+    private static String tasks[] = new String[100];
+    private static int counter = 0;
+
+    public static void setTasks(String task){
+        tasks[counter] = task;
+        counter++;
+    }
+
+    public static String getTasks(){
+        String output = "\t____________________________________________________________";
+        for(int i=0;i<counter;i++){
+            output += "\n\t" + Integer.toString(i+1) + ". " + tasks[i];
+        }
+        output += "\n\t____________________________________________________________";
+        return output;
+    }
     public static void main(String[] args) {
         // String logo = " ____        _        \n"
         //         + "|  _ \\ _   _| | _____ \n"
@@ -11,21 +26,26 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String input;
-        System.out.println("\n\t____________________________________________________________" 
+        System.out.println("\t____________________________________________________________" 
         +"\n\tHello! I'm Duke\n\tWhat can I do for you?"
         + "\n\t____________________________________________________________");
 
         do {
-            input = sc.next();
+            input = sc.nextLine();
             switch(input){
                 case "bye":
-                    System.out.println("\n\t____________________________________________________________" + 
+                    System.out.println("\t____________________________________________________________" + 
                     "\n\tBye. Hope to see you again soon!" +
                     "\n\t____________________________________________________________");
                     break;
+                case "list":
+                    System.out.println(getTasks());
+                    break;
                 default:
-                    System.out.println("\n\t____________________________________________________________\n\t" 
-                    + input 
+                    setTasks(input);
+                    System.out.println("\t____________________________________________________________\n\t"
+                    + "added: "
+                    + input
                     + "\n\t____________________________________________________________");
             }
         }
