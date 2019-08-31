098767f40c9e4dc4686e5a92f1436ef668630a92
e0260237 <e0260237@u.nus.edu>
2019-08-31 13:26:06 +0800

    Task 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 public class Duke {
@@ -10,10 +11,19 @@ public class Duke {
             System.out.println("Hello from\n" + logo);
             System.out.println("Hello! I'm Duke\nWhat can I do for you?");
             String inData = null;
-            while (!Objects.equals(inData, "bye")) {
-                Scanner scan = new Scanner(System.in);
-                inData = scan.nextLine();
-                System.out.println(inData);
+            ArrayList<String> arlist = new ArrayList<String>( );
+            while (!Objects.equals(inData, "bye")){
+                    Scanner scan = new Scanner(System.in);
+                    inData = scan.nextLine();
+                    if(Objects.equals(inData, "list")){
+                    System.out.println(arlist);
+                    }
+                    if(!Objects.equals(inData, "list")){
+                        if(!Objects.equals(inData, "bye")) {
+                            System.out.println("added:" + inData);
+                            arlist.add(inData + "\n");
+                        }
+                }
             }
             System.out.println("Bye. Hope to see you again soon!");
         }
