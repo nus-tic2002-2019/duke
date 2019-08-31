98eec70a3740c0a6711cd742313e334d0265e6d7
e0260237 <e0260237@u.nus.edu>
2019-08-31 12:36:34 +0800

    Task 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
+        public static void main(String[] args) {
+            String logo = " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+            System.out.println("Hello from\n" + logo);
+            System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+            String inData = null;
+            while (!Objects.equals(inData, "bye")) {
+                Scanner scan = new Scanner(System.in);
+                inData = scan.nextLine();
+                System.out.println(inData);
+            }
+            System.out.println("Bye. Hope to see you again soon!");
+        }
 }
