c89c2ae9764415fb6324048078c81130811d32bb
James <jamessoh11@gmail.com>
2019-08-31 18:38:28 +0800

    Implemented level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,8 +8,11 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        System.out.println(hyphen_print("What can I do?"));
+        System.out.println(hyphen_print("What can I do for you?"));
         String response;
+        // initialise string array to store responses
+        String[] my_list = new String[100];
+        int idx = 0;
         // get input from user
         while(true){
             Scanner in = new Scanner(System.in);
@@ -18,7 +21,15 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!\n");
                 return;
             }
-            System.out.println(hyphen_print((response)));
+            if(response.toLowerCase().equals("list")){
+                for(int i = 0; i < idx; i ++){
+                    System.out.println(i+1 + ". " + my_list[i]);
+                }
+            }
+            System.out.println(hyphen_print(("Added: " + response)));
+            my_list[idx] = response;
+            idx ++;
+
         }
     }
 }
