eaea767fc4e6c1c092194bcf50d856340baa516b
Marcus Ng <e0260198@u.nus.edu>
2019-09-04 01:38:25 +0800

    Level-3 + Implementing methods on public class Task.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,23 +5,6 @@ import subclass.*;
 
 public class Duke {
 
-    /*public static String[] add_task(String task){
-        String[] tasklist = new String[100];
-        int count=0;
-        for (int i=0; i<tasklist.length; i++){
-            tasklist[i] = task;
-            count++;
-            System.out.println(Arrays.toString(tasklist));
-        }
-        return Arrays.copyOf(tasklist, count);
-    }*/
-
-    /*public static void print_list(){
-
-    }*/
-
-
-
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -37,48 +20,36 @@ public class Duke {
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t_________________________________________");
-        String[] inputs = new String[100];
+
         line = in.nextLine();
-        int word_count = 0;
+
         while (!line.contains("bye")) {
-            Task newTask = new Task(line);
+           Task newTask = new Task(line);
 
             //add inputs into array
             if (!line.contains("list") && !line.contains("done")){
-                inputs[word_count] = line;
+                //inputs[word_count] = line;
+                newTask.add_task(new Task(line));
                 System.out.println("\t_________________________________________");
                 System.out.println("\tadded: " + line);
                 System.out.println("\t_________________________________________");
-                word_count++;
+                //word_count++;
 
             }
                 //displaying list
             if (line.contains("list")) {
-                System.out.println("\t_________________________________________");
-                String[] print_array = Arrays.copyOf(inputs, word_count);
-                int num = 1;
-                for (String item : print_array) {
-                    System.out.println("\t"+ num + ". "+ "[" + newTask.getStatusIcon() + "] " +print_array[num-1]);
 
-                    num++;
-
-                }
-                System.out.println("\t_________________________________________");
+                newTask.getList();
             }
 
             if (line.contains("done")) {
-                String option = null;
+
                 String[] input_items = line.split(" ");
                 String[] new_array = new String[1];
-                String[] print_array = Arrays.copyOf(inputs, word_count);
                 new_array[0] = input_items[1];
-                option = new_array[0];
+                String option = new_array[0];
                 int task_option = Integer.parseInt(option);
-                newTask.setDone(true);
-                System.out.println("\tNice! I've marked this task as done:");
-                System.out.println("\t[" + newTask.getStatusIcon() + "] " + print_array[task_option-1]);
-                System.out.println("\t_________________________________________");
-
+                newTask.markDone(task_option);
             }
 
             line = in.nextLine();
--- src/main/java/subclass/Task.java
+++ src/main/java/subclass/Task.java
@@ -1,8 +1,39 @@
 package subclass;
 
+import java.util.Arrays;
+
 public class Task {
     protected String description;
     protected boolean isDone;
+
+    private static Task[] inputs = new Task[100];
+    private static int word_count = 0;
+
+    public static void add_task(Task description){
+        inputs[word_count] = description;
+        word_count++;
+    }
+
+    public static void getList() {
+        System.out.println("\t_________________________________________");
+        Task[] print_array = Arrays.copyOf(inputs, word_count);
+        int num = 1;
+        for (Task item : print_array) {
+            System.out.println("\t"+ num + ". "+ "[" + print_array[num-1].getStatusIcon() + "] " +print_array[num-1].toString());
+
+            num++;
+
+        }
+        System.out.println("\t_________________________________________");
+    }
+
+    public static void markDone(int taskNo) {
+        inputs[taskNo-1].setDone(true);
+        Task[] print_array = Arrays.copyOf(inputs, word_count);
+        System.out.println("\tNice! I've marked this task as done:");
+        System.out.println("\t[" + print_array[taskNo-1].getStatusIcon() + "] " + print_array[taskNo-1]);
+        System.out.println("\t_________________________________________");
+    }
 
     public Task(String description) {
         this.description = description;
@@ -11,13 +42,15 @@ public class Task {
 
     public void setDone(boolean isDone) {
         this.isDone = isDone;
+
     }
 
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
-    /*public void printsample() {
-        System.out.println("hoho");
-    }*/
+
+    public String toString() {
+        return this.description;
+    }
 }
