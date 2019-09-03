1c28a485df43f66ad47ef62c8223740b476a72bd
Ivan Ho <ivanhlb@hotmail.com>
2019-09-03 13:27:41 +0800

    implemented Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
+        String[] tmp = new String[100];
+        int size = 0;
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
         while(true)
@@ -14,8 +16,18 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
+            else if(inputStr.toLowerCase().equals("list"))
+            {
+                for(int i = 0; i < size; i++)
+                {
+                    System.out.format("%d: %s\n", i + 1, tmp[i]);
+                }
+            }
             else
-                System.out.println(inputStr);
+            {
+                System.out.println("added: " + inputStr);
+                tmp[size++] = inputStr;
+            }
         }
     }
 }
