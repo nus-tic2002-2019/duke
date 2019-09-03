0ad3da3326c260ff0351056ffb7892bc30c5e240
jkbook09 <jkbook09@gmail.com>
2019-09-03 16:49:18 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        String line;
+
+        while(true) {
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if (line.toUpperCase().equals("BYE")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            } else
+            {
+                System.out.println(line);
+            };
+
+        }
     }
 }
