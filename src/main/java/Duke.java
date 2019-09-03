32cdf176837e2cddad7ee4c6bf320154f6530f73
Ivan Ho <ivanhlb@hotmail.com>
2019-09-03 12:49:56 +0800

    Level 1 first implementation.
    gitignore update
    
    fix for gitignore
    
    test

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?\n");
+        while(true)
+        {
+            String inputStr = input.nextLine();
+            if(inputStr.toLowerCase().equals("bye"))
+            {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else
+                System.out.println(inputStr);
+        }
     }
 }
