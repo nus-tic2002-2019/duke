59269d8030debd8252fc2d92d695a58e90fddbec
Guo Yang <ariesyang92@gmail.com>
2019-09-03 19:50:18 +0800

    added scanner and while loop

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ____        _        \n" 
+                    + "|  _ \\ _   _| | _____ \n" 
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n" 
+                    + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("\n");
+        System.out.println("what can i do for you?\n");
+        String line;
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("list")) {
+                System.out.println("in if\n");
+                in.close();
+            } else {
+                System.out.println("in else\n");
+            }
+            
+        }
     }
+
 }
