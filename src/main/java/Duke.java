34501b3ef8694a4cc31aba968837fd4018f899eb
Kai <tankaiwei@gmail.com>
2019-09-03 18:00:56 +0800

    task2 implement list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,31 @@ import java.util.Scanner;
 
 public class Duke {
 
+    public static String[] store = new String[100];
+    public static int current = 0;
+
+    public static void printItem(String[] items){
+        for(int i = 0;i < current; i++){
+            System.out.println(Integer.toString(i + 1) + ". " + items[i]);
+        }
+        System.out.printf("\n");
+    }
+
 
     public static void checkString(String args){
+        String line;
+        Scanner in = new Scanner(System.in);
         if(args.equals("bye")){
+            System.out.println("Bye. Hope to see you again soon!\n");
             return;
+        } else if (args.equals("list")){
+            printItem(store);
+            line = in.nextLine();
+            checkString(line);
         } else {
-            System.out.println(args);
-            String line;
-            Scanner in = new Scanner(System.in);
+            store[current] = args;
+            System.out.println("Added: " + args + "\n");
+            current += 1;
             line = in.nextLine();
             checkString(line);
         }
@@ -18,6 +35,7 @@ public class Duke {
 
 
     public static void main(String[] args) {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -26,7 +44,7 @@ public class Duke {
 
         String line;
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm from\n" + logo + "What can I do for you?");
         line = in.nextLine();
         checkString(line);
     }
