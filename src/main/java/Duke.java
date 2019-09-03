df583f58fd824e17c6101a8f6806fe0e5c191eda
eliseNg <xinyi1108@gmail.com>
2019-09-03 21:22:47 +0800

    LVL 2 done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,30 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String line;
+        String[] list = new String[10];
+        Scanner in = new Scanner(System.in);
+        System.out.print("Hello! I'm Duke\n" + "What can I do for you?");
+        int index = 0;
+        while (true){
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else if (line.equals("list")) {
+                for (int i = 0; i < index; i++){
+                    System.out.println(i+1 + ". " + list[i]);
+                }
+            }
+            else{
+                System.out.println("added: " + line);
+                list[index] = line;
+                index ++;
+            }
+
+        }
     }
+
 }
+
