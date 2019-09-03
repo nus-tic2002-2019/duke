3f818d401e6e46b62c492d67f3d0bda561b8d708
ahjimomo <kokwoon.ng@gmail.com>
2019-09-03 19:20:06 +0800

    Greet, Exit and Echo!

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;                               //Scanner library
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,13 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+
+        //Level-1
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\n What can I do for you?");
+
+        String reply = myObj.nextLine();                //Reads the next line which is the input
+        System.out.println(reply);
     }
 }
