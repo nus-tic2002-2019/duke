e88c3ce0777d0d5f579984c84eb72f7746a853ec
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-09-04 15:30:11 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,22 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from \n" + logo + "What can I do for you?" );
+		
+		// Level-1
+		String line;
+		while (true){   
+        // Accepts text
+		Scanner in = new Scanner(System.in);
+		line = in.nextLine();
+        //Check entered text for bye
+			if (!"bye".equals(line)){
+				System.out.println("   " + line + "\n");
+			} else {
+				System.out.println("Bye. Hope to see you again soon!");
+				break;
+			}
+		}
     }
 }
+
