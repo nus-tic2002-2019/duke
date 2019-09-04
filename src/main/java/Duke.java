f1d60ad93f1adde71b160bf3b69193fef43f5db8
kohks <kohks@live.com>
2019-09-04 23:10:41 +0800

    Completed Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,21 +14,36 @@ public class Duke {
         System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
         System.out.println("\t--------------------------------------------------");
 
-        Boolean isNotBye = Boolean.TRUE;
+        Boolean isNotBye  = Boolean.TRUE;
+        Boolean isNotList = Boolean.TRUE;
+        String bye_word  = "bye";
+        String list_word = "list";
         String line = "";
-        String stop_word = "bye";
+        String[] stringList = new String[50];
+        int count = 0;
+
         Scanner in = new Scanner(System.in);
 
         while( isNotBye ){
             line = in.nextLine();
-            isNotBye = !(line.equalsIgnoreCase(stop_word));
+            isNotBye  = !(line.equalsIgnoreCase(bye_word));
+            isNotList = !(line.equalsIgnoreCase(list_word));
+
             System.out.println("\t--------------------------------------------------");
-            if (isNotBye){
+
+            if (isNotBye && isNotList){
                 System.out.println("\t"+line);
+                stringList[count] = line;
+                count += 1;
+            } else if ( !isNotList ) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println("\t" + (i+1) + ". " + stringList[i]);
+                }
             } else {
                 System.out.println("\tBye. Hope to see you again soon!");
             }
             System.out.println("\t--------------------------------------------------");
         }
+        stringList = Arrays.copyOf(stringList, count);
     }
 }
