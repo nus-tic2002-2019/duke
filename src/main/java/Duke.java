f3fafd9d373d627b037438d9252ee69df2d79147
YSG_SG <e0373976@u.nus.edu>
2019-09-04 16:27:43 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import javax.swing.plaf.synth.SynthTabbedPaneUI;
 import java.util.Arrays;
 import java.math.RoundingMode;
 import java.awt.*;
+import java.util.*;
+import java.util.Vector;
 import java.util.Scanner;
 
 public class Duke {
@@ -38,6 +40,40 @@ public class Duke {
         }
     }
 
+    public static void  Print_List(Vector<String> List){
+        List.remove(List.size()-1);
+        for(int i = 0; i < List.size(); i++){
+            int j = i+1;
+            System.out.println(j + ". " + List.get(i));
+        }
+    }
+
+    public static void chatting_Vector(Vector<String> List){
+        String Ending = "bye";
+        //Vector<String> List = new Vector<String>();
+        Scanner in = new Scanner(System.in);
+        List.add(in.nextLine());
+
+        Seperated_Line();
+        String last = List.lastElement();
+
+        if(last.equals(Ending)){
+            System.out.println("Bye. Hope to see you again soon!");
+            Seperated_Line();
+            return;
+        }
+        else if (last.equals("list")){
+            Print_List(List);
+            Seperated_Line();
+            chatting_Vector(List);
+        }
+        else{
+            System.out.println("added: " + last);
+            Seperated_Line();
+            chatting_Vector(List);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -45,12 +81,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        //system("CLS");
+
+        Vector<String> List = new Vector<String>();
 
         Seperated_Line();
         Greeting();
         Seperated_Line();
 
-        chatting();
+        chatting_Vector(List);
     }
 }
