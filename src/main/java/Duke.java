1a6c036cfd9a97d6c18d1c80e951c01ec964dfc3
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-09-04 16:19:09 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class Duke {
+public class Duke { 
+	
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,19 +9,29 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from \n" + logo + "What can I do for you?" );
+		String[] toDoList = new String[100];
 		
 		// Level-1
 		String line;
+		int i = 0;
 		while (true){   
         // Accepts text
 		Scanner in = new Scanner(System.in);
 		line = in.nextLine();
         //Check entered text for bye
-			if (!"bye".equals(line)){
-				System.out.println("   " + line + "\n");
-			} else {
-				System.out.println("Bye. Hope to see you again soon!");
+			if ("bye".equals(line)){
+				System.out.println("	Bye. Hope to see you again soon!");
 				break;
+			}
+			if ("list".equals(line)){
+				for (int a=1; a<i+1;a++){
+				System.out.println("	"+a+". "+toDoList[a-1]);
+				}
+			} else {
+				// Level-2
+				toDoList[i] = line;
+				System.out.println("	added: " + line + "\n");
+				i++;
 			}
 		}
     }
