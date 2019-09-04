90c095ea49c1cb149bba8d30e67e324d14c39b4b
Guo Yang <ariesyang92@gmail.com>
2019-09-04 13:13:40 +0800

    level two

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,18 +10,48 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("\n");
         System.out.println("what can i do for you?\n");
+        getMsg();
+    }
+
+    //////////////////////
+    //getting user input//
+    //////////////////////
+    public static void getMsg(){
         String line;
         Scanner in = new Scanner(System.in);
+        String[] messageArr = new String[100]; 
+        int counter = 1;
         while (true) {
             line = in.nextLine();
             if (line.equals("list")) {
-                System.out.println("in if\n");
+                printListFunction(messageArr, counter);
+            } 
+            else if(line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
                 in.close();
-            } else {
-                System.out.println("in else\n");
             }
-            
+            else{
+                storeFunction(messageArr, line, counter);
+                counter ++;
+            }
         }
     }
 
+    /////////////////////////////
+    //store user input into arr//
+    /////////////////////////////
+    public static String[] storeFunction(String[] messageArr, String line, int counter){
+        messageArr[counter] = line;
+        System.out.println("added: " + line);
+        return messageArr;
+    }
+
+    ////////////////////////////////
+    //print out item in stored arr//
+    //////////////////////////////// 
+    public static void printListFunction(String[] messageArr, int counter){
+        for(int i = 1; i < counter; i++){
+            System.out.println(i + "." + messageArr[i]);
+        }
+    }
 }
