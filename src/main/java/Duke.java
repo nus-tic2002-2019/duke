2d79a376b7e60c7ed8acff9a7310ffad1afea72f
lim <e0374012@u.nus.edu>
2019-09-05 22:35:43 +0800

    Level2_Echo_Add_List_func

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,47 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    private static String seperatorLine = "___________________________________________\n";
+    private static String seperatorLine2 = "________________________________________\n";
+
+
+    public static void Echo_Added_List(Scanner input) {
+
+        boolean isBye = false;
+        boolean isList;
+        int listCount = 0, listPrint, count = 0;
+        String userInput;
+        String[] listArray = new String[100];
+
+        while (!isBye) {
+
+            userInput = input.nextLine();
+
+            isList = userInput.equals("list");
+            isBye = userInput.equals("bye");
+
+            if(!isList && !isBye){
+                System.out.print("   " + seperatorLine2);
+                System.out.println("      added:" + userInput);
+                System.out.print("   " + seperatorLine2);
+
+                listArray[listCount] = userInput;
+                listCount++;
+            }
+
+            else if (isList){
+                count = listCount;
+                System.out.print("   " + seperatorLine2);
+
+                for (listPrint = 0; listPrint < count; listPrint++) {
+                    System.out.println("     " + listPrint + ". " + listArray[listPrint]);
+                }
+                System.out.print("   " + seperatorLine2);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,95 +50,17 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        String seperatorLine = "___________________________________________\n";
-        String line;
-        boolean isSame;
-        String[] listArray = new String[100];
-        int i = 0, j =0, count = 0;
-        boolean listSame = false;
-
         System.out.print(seperatorLine);
         System.out.println("Hello! I'm Duke\n"
                 + "What can I do for you");
         System.out.println(seperatorLine);
 
         Scanner input = new Scanner(System.in);
-        line = input.nextLine();
-        listArray[i] = line;
-        isSame = line.equals("bye");
 
-        while (!isSame) {
-
-            line = input.nextLine();
-
-            listSame = line.equals("list");
-
-            if(listSame == false){
-                System.out.print("   " + seperatorLine);
-                System.out.println("      added:" + line);
-                System.out.print("   " + seperatorLine);
-//                line = input.nextLine();
-                isSame = line.equals("bye");
-                listArray[i] = line;
-                i++;
-
-            }
-
-            else {
-//              listSame = line.equals("list");
-
-                count = i;
-                for (j = 0; j < count; j++) {
-                    System.out.println(j + ": " + listArray[j]);
-                }
-
-//                line = input.nextLine();
-                i++;
-                listArray[i] = line;
-                isSame = line.equals("bye");
-            }
-        }
-
+        Echo_Added_List(input);
 
         System.out.print(seperatorLine);
         System.out.println("       " + "Bye. Hope to see you again soon!");
         System.out.println(seperatorLine);
     }
 }
-
-
-
-
-
-
-
- /*while (!isSame) {
-
-         listSame = line.equals("list");
-
-         if(listSame == false){
-         System.out.print("   " + seperatorLine);
-         System.out.println("      added:" + line);
-         System.out.print("   " + seperatorLine);
-         line = input.nextLine();
-         i++;
-         isSame = line.equals("bye");
-         listArray[i] = line;
-//            System.out.println(i);
-         }
-
-         else {
-//              listSame = line.equals("list");
-
-         count = i;
-         for (j = 0; j < count; j++) {
-        System.out.println(j + ": " + listArray[j]);
-        }
-
-        line = input.nextLine();
-        i++;
-        listArray[i] = line;
-        isSame = line.equals("bye");
-//                System.out.println(i);
-        }
-        }*/
