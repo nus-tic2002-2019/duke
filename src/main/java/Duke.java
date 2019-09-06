de74e02c5ca5bc288276b249c29aa9de444b0f58
e0260237 <e0260237@u.nus.edu>
2019-09-07 00:20:06 +0800

    Level 3 v1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -13,22 +14,34 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String inData = null;
         ArrayList<String> arlist = new ArrayList<String>( );
+
         int count = 0;
         while (!Objects.equals(inData, "bye")){
             Scanner scan = new Scanner(System.in);
             inData = scan.nextLine();
             if(Objects.equals(inData, "list")){
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i+1) + "." + arlist.get(i));
+                    Task T = new Task(arlist.get(i));
+                    System.out.println((i+1) + "." + T.toString());
                 }
             }
-            if(!Objects.equals(inData, "list")){
-                if(!Objects.equals(inData, "bye")) {
-                    System.out.println("added:" + inData);
-                    arlist.add(inData);
-                    count++;
-                }
+            else if(Objects.equals(inData.substring(0,4), "done")){
+                int Marked = (Integer.parseInt(inData.substring(inData.indexOf("done")+5, inData.length())));
+                Task T = new Task(arlist.get(Marked-1));
+                T.markAsDone();
+                System.out.println("Nice! I've marked this task as done: \n" + T.toString());
             }
+            else if(!Objects.equals(inData, "list")){
+                    if(!Objects.equals(inData, "bye")) {
+                            if(!Objects.equals(inData.substring(0,4), "done")) {
+                            System.out.println("added:" + inData);
+                            arlist.add(inData);
+                            count++;
+                        }
+                }
+
+            }
+
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
