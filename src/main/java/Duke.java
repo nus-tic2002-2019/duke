81ca3961e01ffde66ce85066b1c500ab300afa3b
e0260237 <e0260237@u.nus.edu>
2019-09-07 14:22:26 +0800

    Level 5 Except Handling v1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,65 +13,69 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String inData = null;
-        ArrayList<Task> arlist = new ArrayList<Task>( );
+        ArrayList<Task> arlist = new ArrayList<Task>();
         Task T = null;
         int count = 0;
 
-        while (!Objects.equals(inData, "bye")){
+        while (!Objects.equals(inData, "bye")) {
             Scanner scan = new Scanner(System.in);
             inData = scan.nextLine();
-            if(Objects.equals(inData, "list")){
+            if (Objects.equals(inData, "list")) {
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i+1) + "." + arlist.get(i).toString());
+                    System.out.println((i + 1) + "." + arlist.get(i).toString());
                 }
-            }
-            else if(inData.contains("done")){
-                int Marked = (Integer.parseInt(inData.substring(inData.indexOf("done")+5, inData.length())));
-                T = arlist.get(Marked-1);
+            } else if (inData.contains("done")) {
+                int Marked = (Integer.parseInt(inData.substring(inData.indexOf("done") + 5, inData.length())));
+                T = arlist.get(Marked - 1);
                 T.markAsDone();
                 System.out.println("Nice! I've marked this task as done: \n" + T.toString());
-            }
-            else if(!Objects.equals(inData, "list")){
-                    if(!Objects.equals(inData, "bye")) {
-                            if(!Objects.equals(inData.substring(0,4), "done")) {
+            } else if (!Objects.equals(inData, "list")) {
+                if (!Objects.equals(inData, "bye")) {
+                    if (!Objects.equals(inData.substring(0, 4), "done")) {
 
-                                if(inData.contains("todo")){
-                                    String itemName = inData.substring(4, inData.length());
-                                    ToDo To = new ToDo(itemName);
-                                    count++;
-                                    arlist.add(To);
-                                    System.out.println("Got it. I've added this task: \n" + To.toString() + "\nNow you have " + count + " tasks in list.");
-
-                                }
-                                else if(inData.contains("deadline")){
-                                    int dividerPosition = inData.indexOf("/");
-                                    String itemName = inData.substring(8, dividerPosition);
-                                    String itemName1 = inData.substring(dividerPosition,inData.length());
-                                    String itemName2 = itemName1.replace("/", "");
-                                    Deadline D = new Deadline(itemName,itemName2);
-                                    count++;
-                                    arlist.add(D);
-                                    System.out.println("Got it. I've added this task: \n" + D.toString() + "\nNow you have " + count + " tasks in list.");
-                                }
-                                else if(inData.contains("event")) {
-                                    int dividerPosition = inData.indexOf("/");
-                                    String itemName = inData.substring(5, dividerPosition);
-                                    String itemName1 = inData.substring(dividerPosition, inData.length());
-                                    String itemName2 = itemName1.replace("/", "");
-                                    Event E = new Event(itemName, itemName2);
-                                    count++;
-                                    arlist.add(E);
-                                    System.out.println("Got it. I've added this task: \n" + E.toString() + "\nNow you have " + count + " tasks in list.");
-                                }
+                        if (inData.contains("todo")) {
+                                String itemName = inData.substring(4, inData.length());
+                                ToDo To = new ToDo(itemName);
+                                count++;
+                                arlist.add(To);
+                                System.out.println("Got it. I've added this task: \n" + To.toString() + "\nNow you have " + count + " tasks in list.");
+                        } else if (inData.contains("deadline")) {
+                            try {
+                                int dividerPosition = inData.indexOf("/");
+                                String itemName = inData.substring(8, dividerPosition);
+                                String itemName1 = inData.substring(dividerPosition, inData.length());
+                                String itemName2 = itemName1.replace("/", "");
+                                Deadline D = new Deadline(itemName, itemName2);
+                                count++;
+                                arlist.add(D);
+                                System.out.println("Got it. I've added this task: \n" + D.toString() + "\nNow you have " + count + " tasks in list.");
+                            } catch (IndexOutOfBoundsException e) {
+                                System.out.println("☹ OOPS!!! The description of a deadline's date needs a / after description");
+                            }
+                        } else if (inData.contains("event")) {
+                            try {
+                                int dividerPosition = inData.indexOf("/");
+                                String itemName = inData.substring(5, dividerPosition);
+                                String itemName1 = inData.substring(dividerPosition, inData.length());
+                                String itemName2 = itemName1.replace("/", "");
+                                Event E = new Event(itemName, itemName2);
+                                count++;
+                                arlist.add(E);
+                                System.out.println("Got it. I've added this task: \n" + E.toString() + "\nNow you have " + count + " tasks in list.");
+                            } catch (IndexOutOfBoundsException e) {
+                                System.out.println("☹ OOPS!!! The description of a event's date needs a / after description");
+                            }
 
                         }
+                    }
+
                 }
 
             }
-
+            System.out.println("Bye. Hope to see you again soon!");
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
+
 
 
