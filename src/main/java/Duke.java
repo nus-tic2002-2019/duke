ff29386cc9482bc016dfedcac298b90b5de43a8f
Casopher <43214601+Casopher@users.noreply.github.com>
2019-09-07 15:55:41 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,38 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        welcome();
+        
+        String line = "";
+        Scanner in = new Scanner(System.in);
+        
+        while ( true ){
+            line = in.nextLine();
+            if ( line.toLowerCase().equals("bye") ){
+                exit();
+                break;
+            }
+            response(line);
+        }
+        
     }
-    System.out.println("Hellow World!")!
+    
+    public static void printLine(){
+        System.out.println("\t____________________________________________________________");
+    }
+    
+    public static void welcome(){
+        printLine();
+        System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?\n");
+        printLine();
+    }
+    public static void exit(){
+        response("Bye. Hope to see you again soon!");
+    }
+    
+    public static void response(String line){
+        printLine();
+        System.out.printf("\t%s\n",line);
+        printLine();
+    }
 }
