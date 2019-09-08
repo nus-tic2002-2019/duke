15663693e0632223aa857b6d245dcf2471f4971c
TanChiaChun <e0261319@u.nus.edu>
2019-09-09 06:02:04 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void greet() {
+        String greet = "____________________________________________________________\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________";
+
+        System.out.println(greet);
+    }
+
+    public static void echoOrExit() {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        if (input.equals("bye") ) {
+            String bye = "____________________________________________________________\n"
+                    + "Bye. Hope to see you again soon!\n"
+                    + "____________________________________________________________";
+            System.out.println(bye);
+            return;
+        }
+
+        String echo = "____________________________________________________________\n"
+                + input
+                + "\n____________________________________________________________";
+        System.out.println(echo);
+
+        echoOrExit();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +37,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        greet();
+        echoOrExit();
     }
 }
