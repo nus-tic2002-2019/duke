133a715e9a903a8c929de4f9aee545b0326c0b4c
E0260261 <e0260261@u.nus.edu>
2019-09-09 20:56:40 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -11,12 +12,13 @@ public class Duke {
         System.out.println("____________________________________________________________\n" + "Hello! I'm Duke\n" + "What can I do for you?\n" + "____________________________________________________________\n");
         Scanner input = new Scanner(System.in);
         String text = "";
+        ArrayList<String> list = new ArrayList<String>();
         while ( !text.equals("bye") ) {
             text = input.nextLine();
             switch ( text ) {
-                case "bye":  System.out.println("____________________________________________________________\n" + "Bye. Hope to see you again soon!\n" + "____________________________________________________________");
-                    break;
-                default: System.out.println("____________________________________________________________\n" + text +"\n" + "____________________________________________________________");
+                case "bye":  System.out.println("____________________________________________________________\n" + "Bye. Hope to see you again soon!\n" + "____________________________________________________________"); break;
+                case "list": System.out.println("____________________________________________________________"); for (int i = 0; i < list.size(); i++){System.out.println((i+1)+". "+list.get(i));} System.out.println("____________________________________________________________\n"); break;
+                default: System.out.println("____________________________________________________________\n" + "added: " + text +"\n" + "____________________________________________________________\n"); list.add(text);
             }
         }
     }
