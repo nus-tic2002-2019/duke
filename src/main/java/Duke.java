d05d1d2be15b37ee561dd4342a0c1ab42dec19a0
Casopher <43214601+Casopher@users.noreply.github.com>
2019-09-09 09:03:24 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,19 +6,44 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+                System.out.println("Hello from\n" + logo);
         welcome();
         
         String line = "";
         Scanner in = new Scanner(System.in);
+        String[] list = new String [100];
+        int index = 0;
         
         while ( true ){
-            line = in.nextLine().toLowerCase();
-            if ( line.equals("bye") ){
+            line = in.nextLine();
+            if ( line.toLowerCase().equals("bye") ){
                 exit();
                 break;
             }
-            response(line);
+            
+            if ( line.toLowerCase().equals("list")){
+                printList(list);
+            } else {  
+                if ( index < 100 ){
+                    list[index]=line;
+                    index += 1;
+                    response("added:" + line);
+                } else {
+                    response("You cannot add any more items. You have exceeded the maximum numer of items on the list.");
+                }
+                
+            }
         }
+                
+        
+    }
+    
+    public static void printList(String list[]){
+        printLine();
+        for ( int i = 0; list[i] != null; i++ ){
+            System.out.printf("\t%d. %s\n",i+1,list[i]);
+        }
+        printLine();
     }
     
     public static void printLine(){
@@ -30,7 +55,6 @@ public class Duke {
         System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?\n");
         printLine();
     }
-    
     public static void exit(){
         response("Bye. Hope to see you again soon!");
     }
@@ -41,3 +65,4 @@ public class Duke {
         printLine();
     }
 }
+
