03d178dda16917630f7ffc4d2bca56a5e4f1a16c
BearTeddy <bearteddy.dvplr@gmail.com>
2019-09-09 17:23:22 +0800

    Resolving Bug between Lvl 2 and Lvl 3
    Signed-off-by: BearTeddy <bearteddy.dvplr@gmail.com>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,6 +21,11 @@ public class Duke {
         System.out.println(HoriLine);
     }
 
+    //check if string numeric
+    public static boolean isNumeric(String s) {
+        return s != null && s.matches("[-+]?\\d*\\.?\\d+");
+    }
+
     // Read the char input of the scanner;
     public static String ReadText() {
         Scanner sc = new Scanner(System.in);
@@ -30,16 +35,33 @@ public class Duke {
     }
 
     public static boolean Task(String Cmmd) {
-        switch (Cmmd.toLowerCase()) {
+        TaskList task = new TaskList();
+        ArrayList<String> commands = new ArrayList<String>(Arrays.asList(Cmmd.split(" ", 3)));
+        switch (commands.get(0).toLowerCase()) {
             case "bye":
+            case "quit":
+            case "close":
+            case "exit":
                 System.out.println("Bye. Hope to see you again soon!");
                 PrintHL();
                 return false;
+            case "list":
+                if (isNumeric(commands.get(1))) {
+                    task.ListTask(Integer.parseInt(commands.get(1)));
+                } else {
+                    task.ListTask();
+                }
+                break;
+            case "add":
+                if (commands.get(1) == "-t" || commands.get(1) == "task") {
+                    task.AddTask(commands.get(2));
+                }
+                break;
             default:
-                System.out.println(Cmmd);
+                System.out.println("Unknown Command : " + Cmmd);
                 PrintHL();
-                return true;
         }
+        return true;
     }
 
     public static void main(String[] args) {
--- src/main/java/MyClasses/TaskList.java
+++ src/main/java/MyClasses/TaskList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class TaskList {
     protected ArrayList<String> taskList = new ArrayList<String>(100);
-
+    protected ArrayList<Boolean> taskStatus = new ArrayList<Boolean>(100);
     //Getter
     public void ListTask() {
         for (String task : this.taskList) {
@@ -15,16 +15,19 @@ public class TaskList {
     //Print First x number of tasks
     public void ListTask(int LoopCnt) {
         for (int x = 0; x < LoopCnt; x++) {
-            System.out.println(this.taskList.get(x));
+            System.out.println(x + ". " + this.taskList.get(x));
         }
     }
 
     public void AddTask(String Task) {
         this.taskList.add(Task);
+        System.out.println("Added : task" + Task);
     }
 
     public void RemoveTask(int index) {
+        String task_item = this.taskList.get(index);
         this.taskList.remove(index);
+        System.out.println("Removed task " + task_item);
     }
 
 }
