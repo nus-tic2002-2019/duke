0d74e6ff83c3ff773d62029ca8a1d023bf5c9eeb
aimee-ch4 <e0374318@u.nus.edu>
2019-09-10 19:59:30 +0800

    Level 2 - Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,57 +1,44 @@
 import java.util.Scanner;
+//import java.util.Arrays;
 
 public class Duke {
 
     public static void addText(String[] arr, String userText, int ctr) {
-        //String[] arr = new String[10];
         arr[ctr] = userText;
-        System.out.println("added:" + userText);
+        System.out.println("added: " + userText);
     }
 
-    public static void listArr(String[] arr) {
+    public static void listArr(String[] arr, int ctr) {
         int i = 0;
-        while (!arr[i].equals("")) {
-            System.out.println(arr[i]);
+        while (i < ctr) {
+            System.out.println((i+1) + ": " + arr[i]);
             i++;
         }
     }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
 
-    System.out.println("Hello from\n" + logo);*/
-    String userInput;
-    Scanner in = new Scanner(System.in);
-    //Level 1
+        //System.out.println("Hello from\n" + logo);*/
+        String userInput;
+        Scanner in = new Scanner(System.in);
         System.out.println("Hello, I'm Duke.\nWhat can I do for you?");
 
-    String[] userArr = new String[10];
-    int i = 0;
-    userInput = in.nextLine();
-        while (!userInput.toLowerCase().equals("bye")) {
-        System.out.println(userInput);
+        String[] userArr = new String[100];
+        int i = 0;
         userInput = in.nextLine();
-    }
 
-        if (userInput.toLowerCase().equals("bye")) {
-        System.out.println("Bye. Hope to see you again soon!");
-    }
-
-        /*while (!userInput.toLowerCase().equals("bye") && (i < 10)) {
+        while (!userInput.toLowerCase().equals("bye")) {
             if (userInput.toLowerCase().equals("list")) {
-                listArr(userArr);
+                listArr(userArr, i);
             } else {
                 addText(userArr, userInput, i);
-                userInput = in.nextLine();
                 i++;
             }
-        }*/
+            userInput = in.nextLine();
+        }
 
-}
+        if (userInput.toLowerCase().equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
+    }
 }
