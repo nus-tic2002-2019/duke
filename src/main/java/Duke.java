27d22781a2a49aa61ff68ce734fd159c8c8dd027
Spencer Nah Zhi Hao <spencernzh@gmail.com>
2019-09-10 20:41:22 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +9,36 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        greet();
+        echo();
     }
+
+    public static void dukePrint(String input) {
+        input = input.replace("\n", "\n\t");
+        System.out.println("\t" + input);
+        System.out.println("__________________________________________________________________________\n");
+    }
+
+    public static void greet() {
+        dukePrint("Hello, I'm Duke\nWhat can I do for you?");
+    }
+
+    public static void echo() {
+        Scanner in = new Scanner(System.in);
+
+        while (in.hasNext()) {
+            String input = in.nextLine();
+            switch(input) {
+                case("bye"):
+                    dukePrint("BYEBYE!");
+                    break;
+                default:
+                    dukePrint("added: " + input);
+            }
+        }
+    }
+
 }
+
+
