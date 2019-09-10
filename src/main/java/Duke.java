1d22d0de04898e6c6630aa3437f0d44ae4bbe16c
Yralle <yralleljg@gmail.com>
2019-09-11 03:30:14 +0800

    Level 1 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,17 +6,38 @@
  * @author yralle.lesly.gimpaya
  * @version 0.1
  * @since 2019-09-11
- *
- *
  */
 
+import java.util.Scanner;
+
 public class Duke {
+
+    private static void WelcomeBanner() {
+
+        String logo = " _______   __    __   __  ___  _______ \n"
+                + "|       \\ |  |  |  | |  |/  / |   ____|\n"
+                + "|  .--.  ||  |  |  | |  '  /  |  |__   \n"
+                + "|  |  |  ||  |  |  | |    <   |   __|\n"
+                + "|  '--'  ||  `--'  | |  .  \\  |  |___\n"
+                + "|_______/  \\______/  |__|\\__\\ |_______|\n";
+        System.out.println(logo);
+    }
+
+    public static void line() {
+        System.out.println("   ____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        WelcomeBanner();
+        DukeBrain Duke = new DukeBrain();
+
+        while(Duke.isAlive()) {
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            line();
+            Duke.converse(input);
+
+        }
+
     }
 }
