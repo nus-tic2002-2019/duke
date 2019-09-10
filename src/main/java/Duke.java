6a3d80d3a0bc3c8822867d0e9616ef0a726dd2d6
LShihao <e0166067@u.nus.edu>
2019-09-10 19:12:08 +0800

    testing for overloading toString()

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
+
+    String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke d = new Duke();
+        System.out.println(d);
+
+        //System.out.println("\tHello! I'm Duke \n\tWhat can I do for you?");
+    }
+    @Override
+    public String toString() {
+        return "|"  + "|" + logo;
     }
 }
