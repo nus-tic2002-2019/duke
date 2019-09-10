681a59c273e43c1a16e74c0d68a69bacddd5320b
Dax Tan <this.is.dax@gmail.com>
2019-09-10 21:14:02 +0800

    level 1 - echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void echo() {
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+        while(!line.equals("bye")){
+            System.out.println(line);
+            line = in.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +19,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        echo();
     }
 }
