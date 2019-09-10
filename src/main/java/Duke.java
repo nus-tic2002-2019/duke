b276eba7a81c8d7e1671a3bbc9ad2f23a5285f13
e0260253 <e0260253@u.nus.edu>
2019-09-10 19:19:11 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\n" +" What can I do for you?");
+
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        while(true) {
+            line = in.nextLine();
+            if (line.equals("list")) System.out.println("list");
+            if (line.equals("blah")) System.out.println("blah");
+            if (line.equals("bye"))
+            {
+                System.out.println("Bye. Hope to see you again soon");
+                break;
+            }
+        }
+
     }
 }
