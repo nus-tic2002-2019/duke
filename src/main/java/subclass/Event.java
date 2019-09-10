d6d2ac489098dc41a620f1a9a1bbac2cd32ef03b
Marcus Ng <e0260198@u.nus.edu>
2019-09-10 11:46:34 +0800

    Level-4: complete, added event.class

---
+++ src/main/java/subclass/Event.java
@@ -0,0 +1,14 @@
+package subclass;
+
+public class Events extends Task {
+    protected String at;
+
+    public Events(String description, String at) {
+        super(description);
+        this.at = at;
+    }
+
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + at + ")";
+    }
+}
