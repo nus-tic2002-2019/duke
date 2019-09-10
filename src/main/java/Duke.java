5ae59c5f0953c3b3bab55ef0dd26019ab327688c
Yralle <yralleljg@gmail.com>
2019-09-11 02:21:28 +0800

    first commit w/ command line

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,15 @@
+/**
+ *  <h1>TIC2002 - Introduction to Software Engineering Project</h1>
+ *  The project aims to build a product named Duke,
+ *  a Personal Assistant Chatbot that helps a person to keep track of various things.
+ *
+ * @author yralle.lesly.gimpaya
+ * @version 0.1
+ * @since 2019-09-11
+ *
+ *
+ */
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
