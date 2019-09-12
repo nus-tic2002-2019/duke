51279ebae49ae4d08f4736d5d7ffb3873ddba07a
kohks <kohks@live.com>
2019-09-12 20:04:02 +0800

    added Task class. add recognition for "done x" add printout of task with status.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,31 +14,54 @@ public class Duke {
         System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
         System.out.println("\t--------------------------------------------------");
 
-        Boolean isNotBye  = Boolean.TRUE;
-        Boolean isNotList = Boolean.TRUE;
+        Boolean isNotBye  = Boolean.TRUE;   // check if user type "Bye"
+        Boolean isNotList = Boolean.TRUE;   // check if user type "List"
+        Boolean done = Boolean.FALSE;       // status of task
         String bye_word  = "bye";
         String list_word = "list";
+        String done_word = "done";
         String line = "";
         String[] stringList = new String[50];
+        Task[] t = new Task[50];
         int count = 0;
+        int taskNo = 0;
 
         Scanner in = new Scanner(System.in);
 
         while( isNotBye ){
             line = in.nextLine();
+            String[] word = line.split(" ");
+            done_word = word[0];
+            //taskNo = Integer.parseInt(word[1]);
+
             isNotBye  = !(line.equalsIgnoreCase(bye_word));
             isNotList = !(line.equalsIgnoreCase(list_word));
+            done      = !(line.equalsIgnoreCase(done_word));
+
 
             System.out.println("\t--------------------------------------------------");
 
             if (isNotBye && isNotList){
-                System.out.println("\t"+line);
-                stringList[count] = line;
-                count += 1;
+                if (done){
+                    taskNo = Integer.parseInt(word[1]);
+                    t[taskNo-1].taskDone();
+                    continue;
+                } else {
+
+                    System.out.println("\t" + line);
+                    stringList[count] = line;
+                    t[count] = new Task(line);
+                    count += 1;
+
+                }
+
+
             } else if ( !isNotList ) {
                 for (int i = 0; i < count; i++) {
-                    System.out.println("\t" + (i+1) + ". " + stringList[i]);
+                    //System.out.println("\t" + (i+1) + ". " + stringList[i]);
+                    System.out.println("\t" + (i+1) + "." + "[" + t[i].getStatusIcon() + "]" + t[i].getDescription());
                 }
+
             } else {
                 System.out.println("\tBye. Hope to see you again soon!");
             }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -12,6 +12,13 @@ public class Task {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void taskDone(){
+        this.isDone = true;
+    }
     //...
 
 }
