1e516dc2659637454f1f8a3f0f2239b7a5c76a4f
wangwenwei <e0373972@u.nus.edu>
2019-09-13 01:15:42 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,39 +9,45 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
+
+        printWithLine(List.of("Hello! I'm Duke", "What can I do for you?"));
 
         String line;
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello! I'm Duke" + System.lineSeparator() + "What can I do for you?");
-        //System.out.println("week 4");
-        //line = in.nextLine();
-        //System.out.println(line);
-        String [] item = new String[100];
-        int index = 0;
 
-        //line = in.nextLine();
-        //System.out.println(line);
+        List<Task> item = new ArrayList<>();
+
         do{
-
             line = in.nextLine();
             if (line.equals("list")){
-                int k = 1;
-                for (int i = 0; i< index; i++){
-
-                    System.out.println(k + ". " + item[i]);
-                    k++;
+                List<String>messages = new ArrayList<>();
+                printWithLine(List.of("Here are the tasks in your list: "));
+                for (int i = 0; i< item.size(); i++){
+                    messages.add(i + 1 + "." + item.get(i));
                 }
-            }else{
-                item[index] = line;
-                index++;
-                System.out.println("added: " + line);
+                printWithLine(messages);
+            }else if(line.split(" ")[0].equals("done")){
+                Task markItem = item.get(Integer.parseInt(line.substring(5)) -1);
+                markItem.markAsDone();
+                printWithLine(List.of("Nice! I've marked this task as done:", " " + markItem));
+            } else{
+                Task task = new Task(line);
+                item.add(task);
+                printWithLine(List.of("added: " + task));
             }
-            //item[index] = line;
-            //index++;
-            //System.out.println("added: " + line);
-
         }while(!line.equals("bye"));
-        System.out.println("Bye. Hope to see you again soon!");
+        printWithLine(List.of("Bye. Hope to see you again soon!"));
     }
+
+    private static void printWithLine(List<String> messages){
+        System.out.println("     ________________________");
+        for (String message : messages){
+           System.out.println("     " + message);
+        }
+        System.out.println("     ________________________");
+    }
+
+
 }
