f50a2f04aace7703efb93d3aff2c61f217fb9730
Lai Ping <chilaiping@icloud.com>
2019-09-15 04:04:20 +0800

    Level_1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String horizontal_line =  ("____________________________________\n");
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello from\n" + logo);
+        System.out.println(horizontal_line + "Hello I'm Duke\nWhat can I do for you? \n" + horizontal_line);
+
+        Scanner in = new Scanner(System.in);
+        String line;
+        line = in.nextLine();
+        String end = new String ("bye");
+        while(!line.equals(end)) {
+            System.out.println(horizontal_line + line + '\n'+ horizontal_line);
+            line = in.nextLine();
+        } System.out.println(horizontal_line + "Bye. Hope to see you again soon!" +'\n'+ horizontal_line);
 
     }
 }
