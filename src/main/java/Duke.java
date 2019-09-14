f07912913a8b06d00cee4f890d90eb4435853a3b
lowyanshuang <lowyanshuang@gmail.com>
2019-09-14 15:21:53 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Duke {
 
     public static ArrayList<String> list = new ArrayList<>();
+    public static ArrayList<Boolean> mark = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -16,34 +17,58 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
-        while(exit(echo()));
+        while(printout(echo()));
     }
 
-    public static String echo(){
+      public static String echo(){
         String userInput;
         Scanner scan = new Scanner( System.in );
         userInput = scan.nextLine();
 
-        if(!userInput.equals("list")) {
-            list.add(userInput);
+         if(userInput.contains("done")) {
+             String[] splitStr = userInput.split("\\s+");
+             // System.out.println("ddd " + splitStr[1]);
+             mark.set(Integer.valueOf(splitStr[1])-1,  true);
         }
+         else if(!userInput.equals("list")) {
+             list.add(userInput);
+             mark.add(false);
+         }
         return userInput;
     }
 
-    public static boolean exit(String userInput){
+    public static boolean printout(String userInput){
         if(userInput.equals("bye")){
             System.out.println("Bye. Hope to see you again soon!");
             return false;
         }
         else if(userInput.equals("list")) {
-            for (int i = 0; i < list.size(); i++)
-            {
-                System.out.println(i+1 + ". " + list.get(i) + "\n");
-            }
+                System.out.println("Here are the tasks in your list:");
+
+                for (int i = 0; i < list.size(); i++)
+                {
+                    System.out.println(i+1 + ". [" + getMark(mark.get(i))+ "] " + list.get(i));
+                }
+        }
+        else if(userInput.contains("done")) {
+           // int index = Integer.valueOf(userInput.substring(userInput.length() + 1)) - 1;
+            String[] splitStr = userInput.split("\\s+");
+            int index = Integer.valueOf(splitStr[1])-1;
+            System.out.println("Nice! I've marked this task as done: ");
+            System.out.println("[" + getMark(mark.get(index))+ "] " + list.get(index));
+
         }
         else {
             System.out.println("Added: " + userInput);
         }
         return true;
+    }
+
+    public static String getMark(boolean result) {
+
+        if(result == true)
+            return "✓";
+        else
+             return "X";
     }
 }
