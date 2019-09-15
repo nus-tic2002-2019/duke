155d9a28773e34ea64c208fabf9c8ce8ba3c9b21
Lai Ping <chilaiping@icloud.com>
2019-09-15 22:28:28 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
 import java.util.Scanner;
+
 
 public class Duke {
     public static void main(String[] args) {
-        String horizontal_line =  ("____________________________________\n");
+        String horizontal_line = ("____________________________________\n");
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -14,11 +16,31 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         String line;
         line = in.nextLine();
-        String end = new String ("bye");
-        while(!line.equals(end)) {
-            System.out.println(horizontal_line + line + '\n'+ horizontal_line);
-            line = in.nextLine();
+
+        // Level 2
+        String[] list = new String [50];
+        int count = 0;
+        while (!line.equals("bye")) {
+
+             if(line.equals("list")) {
+                 System.out.print(horizontal_line);
+                 for (int i = 0; i < count ; i++) {
+                     System.out.println(i + 1 + "." + list[i]);
+                 }
+                 System.out.println(horizontal_line);
+                 line = in.nextLine();
+             } else {
+                 list[count] = line;
+                 System.out.println(horizontal_line + "Added: " + list[count] + '\n' + horizontal_line);
+                 line = in.nextLine();
+                 count++;
+             }
         } System.out.println(horizontal_line + "Bye. Hope to see you again soon!" +'\n'+ horizontal_line);
 
+        // Level 1
+        // while(!line.equals(end)) {
+        //    System.out.println(horizontal_line + line + '\n'+ horizontal_line);
+        //    line = in.nextLine();
+        //} System.out.println(horizontal_line + "Bye. Hope to see you again soon!" +'\n'+ horizontal_line);
     }
 }
