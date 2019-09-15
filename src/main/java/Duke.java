ec65522fa3eb27e90bd003c314507d7ee03583ab
remus <remus.sng@gmail.com>
2019-09-15 22:40:08 +0800

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,26 +13,42 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         System.out.print("");
         String line;
-        String result[] = new String[10];
+        //String result[] = new String[10];
+        Task[] taskList = new Task[10];
         int counter = 0;
         line = in.nextLine();
-        while (!line.equals("bye")) {
-            if (!line.equals("list")) {
-                //line = in.nextLine()
-                System.out.println("added: " + line);
-                result[counter] = line;
+        while (true) {
+            if (line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else if (line.contains("done")) {
+                int counter2 = Integer.valueOf(line.substring(5, line.length()));
+                Task t = taskList[counter2 -1];
+                t.markAsDone();
+                System.out.println("Nice! I've marked this as done:");
+                System.out.print("[" + t.getStatusIcon() + "]" + t.description +"\n");
                 line = in.nextLine();
-                counter++;
-            } else if (line.equals("list")) {
+            }
 
+
+            else if (line.equals("list")) {
                 for (int i = 0; i < counter; i++) {
-                    System.out.println(i + 1 + "." + result[i]);
+                    System.out.println(i+1 + "." + "[" + taskList[i].getStatusIcon() + "]" + taskList[i].description);
                 }
                 line = in.nextLine();
             }
-            if (line.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+            else if (!line.equals("list")) {
+                //line = in.nextLine()
+                Task newTask = new Task(line);
+                taskList[counter] = newTask;
+                newTask.description = line;
+                System.out.println("added: " + line);
+                //result[counter] = line;
+                line = in.nextLine();
+                counter++;
             }
+
         }
     }
 }
