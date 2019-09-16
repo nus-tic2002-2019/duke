050b07274725976e541b7785da005c4248e57b6c
Dax Tan <this.is.dax@gmail.com>
2019-09-16 12:35:36 +0800

    level 3 - mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,18 +7,45 @@ public class Duke {
         String line = in.nextLine();
 
         while(!line.equals("bye")){
-            if (!line.equals("list")) {
+            // list
+            if (line.equals("list")) {
+                int i = 1;
+                if (todoList.getItemCount() == 0) {
+                    System.out.println("Your list is empty.");
+                } else {
+                    boolean[] status = todoList.getStatus();
+                    for (String item : todoList.getTask()) {
+                        if (item != null) {
+                            System.out.println(i + "." + "[" + (status[i - 1] ? "✓" : "✗") + "] " + item);
+                            i++;
+                        }
+                    }
+                }
+            // done
+            } else if (line.length() >= 4 && line.substring(0, 4).equals("done")) {
+                try {
+                    int itemNumber = Integer.parseInt(line.substring(5, line.length())) - 1;
+                    if (todoList.doTask(itemNumber) && todoList.getItemCount() > itemNumber) {
+                        // TODO: change task to an array of object class instead?
+                        String[] taskList = todoList.getTask();
+                        boolean[] taskStatus = todoList.getStatus();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("[" + (taskStatus[itemNumber] ? "✓" : "✗") + "] " + taskList[itemNumber]);
+                    } else {
+                        System.out.println("Unable to mark task as done.");
+                    }
+                } catch (NumberFormatException error) {
+                    System.out.println("Unable to mark task as done.");
+                } catch (StringIndexOutOfBoundsException error) {
+                    System.out.println("Unable to mark task as done.");
+                } catch (ArrayIndexOutOfBoundsException error) {
+                    System.out.println("Unable to mark task as done.");
+                }
+            // add task
+            } else {
                 System.out.println(line);
                 if (!todoList.setTask(line)) {
                     System.out.println("Your task list is full, unable to add more task.");
-                }
-            } else {
-                int i = 1;
-                for (String item : todoList.getTask()) {
-                    if (item != null) {
-                        System.out.println(i + ". " + item);
-                        i++;
-                    }
                 }
             }
             line = in.nextLine();
