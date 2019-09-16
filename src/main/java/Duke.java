72c82f05f1c273782166d530d95091c38e9513d2
TanChiaChun <e0261319@u.nus.edu>
2019-09-17 06:03:06 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static String[] list = new String[100];
-    private static int listCounter = 0;
+    private static Task[] tasks = new Task[100];
+    private static int tasksCounter = 0;
 
     public static void addToList(String input) {
-        list[listCounter] = input;
-        listCounter++;
+        tasks[tasksCounter] = new Task(input);
+        tasksCounter++;
     }
 
     public static void greet() {
@@ -18,25 +18,48 @@ public class Duke {
         System.out.println(greet);
     }
 
-    public static void echoOrExitorAdd() {
+    public static void modify() {
+        //Get user input
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
 
-        if (input.equals("bye") ) {
+        if (input.equals("bye") ) { //Bye
             String bye = "____________________________________________________________\n"
                     + "Bye. Hope to see you again soon!\n"
                     + "____________________________________________________________";
             System.out.println(bye);
             return;
-        } else if (input.equals("list") ) {
+        } else if (input.equals("list") ) { //List tasks
             System.out.println("____________________________________________________________");
-            for (int i = 0; i < listCounter; i++) {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < tasksCounter; i++) {
                 System.out.print(i + 1);
-                System.out.print(". ");
-                System.out.println(list[i]);
+                System.out.print(".");
+                System.out.print("[");
+                System.out.print(tasks[i].getStatusIcon() );
+                System.out.print("] ");
+                System.out.println(tasks[i].getDescription() );
             }
             System.out.println("____________________________________________________________");
-        } else {
+        } else if ( (input.substring(0, input.indexOf(' ') ) ).equals("done") ) { //Mark task as done
+            //Get task index
+            String taskIndex = input.substring(input.indexOf(' ') + 1, input.length());
+            int taskNum = Integer.parseInt(taskIndex) - 1;
+
+            //Set task as done
+            tasks[taskNum].setDone();
+
+            System.out.println("____________________________________________________________");
+            System.out.println("Nice! I've marked this task as done:");
+
+            //Print completed task
+            System.out.print("[");
+            System.out.print(tasks[taskNum].getStatusIcon() );
+            System.out.print("] ");
+            System.out.println(tasks[taskNum].getDescription() );
+
+            System.out.println("____________________________________________________________");
+        } else { //Add to task list
             addToList(input);
             String echo = "____________________________________________________________\n"
                     + "added: " + input
@@ -44,7 +67,7 @@ public class Duke {
             System.out.println(echo);
         }
 
-        echoOrExitorAdd();
+        modify(); //Recurring method
     }
 
     public static void main(String[] args) {
@@ -56,6 +79,6 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Bye!");
         greet();
-        echoOrExitorAdd();
+        modify();
     }
 }
