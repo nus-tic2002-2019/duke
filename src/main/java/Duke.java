090ae9b2c9d422bfdc05563fcc10e8954222eaeb
E0260261 <e0260261@u.nus.edu>
2019-09-17 19:41:25 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,13 +12,31 @@ public class Duke {
         System.out.println("____________________________________________________________\n" + "Hello! I'm Duke\n" + "What can I do for you?\n" + "____________________________________________________________\n");
         Scanner input = new Scanner(System.in);
         String text = "";
+        String filteredtext = "";
         ArrayList<String> list = new ArrayList<String>();
         while ( !text.equals("bye") ) {
             text = input.nextLine();
-            switch ( text ) {
+            try {
+                filteredtext = text.substring(0, text.indexOf(' '));
+            } catch (Exception e) {
+                filteredtext = text;
+            }
+            if (filteredtext.equals("done")){
+                System.out.println("____________________________________________________________\n" + "Nice! I've marked this task as done:");
+                //System.out.println("Debugging");
+                String getTaskNum = text.substring(5);
+                //System.out.println("Debug:" + getTaskNum);
+                int doneInt = Integer.parseInt(getTaskNum);
+                String filteredList = list.get(doneInt - 1);
+                list.set(doneInt - 1, ("[" + "\u2713" + "]" + filteredList.substring(3)));
+                System.out.println(list.get(doneInt -1));
+                System.out.println("____________________________________________________________");
+            }
+            switch ( filteredtext ) {
                 case "bye":  System.out.println("____________________________________________________________\n" + "Bye. Hope to see you again soon!\n" + "____________________________________________________________"); break;
-                case "list": System.out.println("____________________________________________________________"); for (int i = 0; i < list.size(); i++){System.out.println((i+1)+". "+list.get(i));} System.out.println("____________________________________________________________\n"); break;
-                default: System.out.println("____________________________________________________________\n" + "added: " + text +"\n" + "____________________________________________________________\n"); list.add(text);
+                case "list": System.out.println("____________________________________________________________"); for (int i = 0; i < list.size(); i++){System.out.println((i+1)+"."+list.get(i));} System.out.println("____________________________________________________________\n"); break;
+                case "done": break;
+                default: System.out.println("____________________________________________________________\n" + "added: " + text +"\n" + "____________________________________________________________\n"); list.add("[" + "\u2718" + "]" + text);
             }
         }
     }
