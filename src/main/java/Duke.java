888a9b254adaa901e58bbd550dc58b2afe41bb46
kohks <kohks@live.com>
2019-09-24 21:43:51 +0800

    Level-6 Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,34 @@
+import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static Task[] task = new Task[100];
     private static Integer taskNo = 0;
+    private static ArrayList<Task> ArrayTask = new ArrayList<Task>();
+    private static Integer listNo = 0;
 
     public static void addTask(Task t) {
         task[taskNo] = t;
         taskNo++;
+        ArrayTask.add(t);
+        listNo++;
+    }
+/*
+    public void saveFile(ArrayList<Task> t)throws FileNotFoundException {
+        FileOutputStream fileoutput = new FileOutputStream("D:\\git\\duke\\task.txt");
+        ObjectOutputStream objectoutput = new ObjectOutputStream(fileoutput);
+        objectoutput.writeObject(t);
+        objectoutput.close();
     }
 
+    public void loadFile(ArrayList<Task> t)throws FileNotFoundException{
+        FileInputStream fis = new FileInputStream("D:\\git\\duke\\task.txt");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        ArrayList<Task> ArrayTask = (ArrayList<Task>)ois.readObject();
+        ois.close();
+    }
+*/
     public static void main(String[] args) {
 
         Message.welcomeMessage(); // Duke welcome message
@@ -17,7 +37,7 @@ public class Duke {
         String user_input = "";
         int index = 0;
 
-        String command = ""; // initialise command to loop while() till "bye"
+        String command = ""; // initialise command to loop while() until "bye"
 
         while (!command.equals(bye_word)) {
             Scanner in = new Scanner(System.in);
@@ -25,13 +45,13 @@ public class Duke {
             command = user_input.split(" ")[0].toLowerCase();
             switch (command) {
                 case "list":
-                    Message.listMessage(task, taskNo);
+                    Message.listMessage(ArrayTask, ArrayTask.size());
                     break;
 
                 case "todo":
                     if (ErrType.TaskCheck(user_input)) {
                         addTask(new Todo(user_input.replace("todo", "").trim()));
-                        Message.acknowledgeMessage(task, taskNo);
+                        Message.acknowledgeMessage(ArrayTask, ArrayTask.size());
                     }
                     break;
 
@@ -39,7 +59,7 @@ public class Duke {
                     if (ErrType.TaskCheck(user_input) && ErrType.ScheduleCheck(user_input)) {
                         addTask(new Deadline(user_input.split("/")[0].replace("deadline ", ""),
                                 user_input.split("/")[1].replace("by", "")));
-                        Message.acknowledgeMessage(task, taskNo);
+                        Message.acknowledgeMessage(ArrayTask, ArrayTask.size());
                     }
                     break;
 
@@ -47,7 +67,7 @@ public class Duke {
                     if (ErrType.TaskCheck(user_input) && ErrType.ScheduleCheck(user_input)) {
                         addTask(new Event(user_input.split("/")[0].replace("event ", ""),
                                 user_input.split("/")[1].replace("at", "")));
-                        Message.acknowledgeMessage(task, taskNo);
+                        Message.acknowledgeMessage(ArrayTask, ArrayTask.size());
                     }
                     break;
 
@@ -61,8 +81,35 @@ public class Duke {
                     break;
 
                 case "delete":
-                    System.out.println("\tyou have arrive at delete function");
+                    index = ErrType.toInteger(user_input.split(" ")[1], ArrayTask.size()); // with Exceptions handling
+                    if (index == -1) {
+                        System.out.println("\tPlease key a valid task number.");
+                        break;
+                    }
+                    Message.deleteMessage(ArrayTask, index);
                     break;
+
+/*
+                case "save":
+                    try {
+                        FileOutputStream fos = new FileOutputStream("D:\\git\\dukeoutput.txt");
+                        ObjectOutputStream oos = new ObjectOutputStream(fos);
+                        oos.writeObject(ArrayTask);
+                        oos.close();
+                    } catch (Exception ex) {
+                    }
+                    break;
+
+                case "load":
+                    try {
+                        FileInputStream fis = new FileInputStream("D:\\git\\dukeoutput.txt");
+                        ObjectInputStream ois = new ObjectInputStream(fis);
+                        ArrayTask = (ArrayList<Task>)ois.readObject();
+                        System.out.println(ArrayTask);
+                    } catch (Exception e) {
+                    }
+                    break;
+*/
 
                 case "bye": // "bye" command will end loop after looping back to while()
                     break;
--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Message {
     public static void welcomeMessage(){
         String logo = " ____        _        \n"
@@ -6,7 +8,6 @@ public class Message {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         System.out.println("\t--------------------------------------------------");
         System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
         System.out.println("\t--------------------------------------------------");
@@ -16,11 +17,11 @@ public class Message {
         System.out.println("\tBye. Hope to see you again soon!");
         System.out.println("\t--------------------------------------------------");
     }
-    public static void acknowledgeMessage(Task[] t, Integer taskNo){
+    public static void acknowledgeMessage(ArrayList<Task> t, Integer index){
         System.out.println("\t--------------------------------------------------");
         System.out.println("\tGot it. I've added this task:");
-        System.out.println("\t" + t[taskNo-1]);
-        System.out.println("\tNow you have " + taskNo + " tasks in the list");
+        System.out.println("\t" + t.get(index-1)); //t[taskNo-1])
+        System.out.println("\tNow you have " + index + " tasks in the list");
         System.out.println("\t--------------------------------------------------");
     }
     public static void doneMessage(Task[] t, Integer index){
@@ -30,11 +31,20 @@ public class Message {
         System.out.println("\t" + t[index-1]);
         System.out.println("\t--------------------------------------------------");
     }
-    public static void listMessage(Task[] t, Integer taskNo){
+    public static void deleteMessage(ArrayList<Task> t, Integer index){
+        //t[index - 1].taskDone();
+        System.out.println("\t--------------------------------------------------");
+        System.out.println("\tNoted! I have removed this task.");
+        System.out.println("\t" + t.get(index-1));
+        System.out.println("\tNow you have " + (t.size()-1) + " tasks in the list");
+        System.out.println("\t--------------------------------------------------");
+        t.remove(index-1);
+    }
+    public static void listMessage(ArrayList<Task> t, Integer index){
         System.out.println("\t--------------------------------------------------");
         System.out.println("\tHere are the tasks in your list:");
-        for (int i = 0; i < taskNo ; i++) {
-            System.out.println("\t" + (i+1) + "." + t[i]);
+        for (int i = 0; i < index ; i++) {
+            System.out.println("\t" + (i+1) + "." + t.get(i));
         }
         System.out.println("\t--------------------------------------------------");
     }
