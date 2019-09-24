872d9984f5db70fa5053f23ea2b36e468866ba22
E0260261 <e0260261@u.nus.edu>
2019-09-24 20:10:54 +0800

    Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,14 +27,25 @@ public class Duke {
                 int doneInt = Integer.parseInt(getTaskNum);
                 doneInt--;
                 String filteredList = list.get(doneInt);
-                list.set(doneInt, ("[" + "\u2713" + "]" + filteredList.substring(3)));
+                //System.out.println(filteredList);
+                //System.out.println(filteredList.indexOf("]["));
+                //int newFilter = filteredList.indexOf("][");
+                if ( filteredList.indexOf("][") == 2 ) {
+                    list.set(doneInt, (filteredList.substring(0, 3) + "[" + "\u2713" + "]" + filteredList.substring(6)));
+                } else {
+                    list.set(doneInt, ("[" + "\u2713" + "]" + filteredList.substring(3)));
+                }
                 System.out.println(list.get(doneInt));
                 System.out.println("____________________________________________________________");
             }
+
             switch ( filteredtext ) {
                 case "bye":  System.out.println("____________________________________________________________\n" + "Bye. Hope to see you again soon!\n" + "____________________________________________________________"); break;
                 case "list": System.out.println("____________________________________________________________"); for (int i = 0; i < list.size(); i++){System.out.println((i+1)+"."+list.get(i));} System.out.println("____________________________________________________________\n"); break;
                 case "done": break;
+                case "todo": list.add("[T][" + "\u2718" + "]" + text); System.out.println("____________________________________________________________\n" + "Got it. I've added this task:\n" + "[T][" + "\u2718" + "]" + text +"\n" + "Now you have " + list.size() + " tasks in the list.\n" + "____________________________________________________________\n"); break;
+                case "event": list.add("[E][" + "\u2718" + "]" + text); System.out.println("____________________________________________________________\n" + "Got it. I've added this task:\n" + "[E][" + "\u2718" + "]" + text +"\n" + "Now you have " + list.size() + " tasks in the list.\n" + "____________________________________________________________\n"); break;
+                case "deadline": list.add("[D][" + "\u2718" + "]" + text); System.out.println("____________________________________________________________\n" + "Got it. I've added this task:\n" + "[D][" + "\u2718" + "]" + text +"\n" + "Now you have " + list.size() + " tasks in the list.\n" + "____________________________________________________________\n"); break;
                 default: System.out.println("____________________________________________________________\n" + "added: " + text +"\n" + "____________________________________________________________\n"); list.add("[" + "\u2718" + "]" + text);
             }
         }
