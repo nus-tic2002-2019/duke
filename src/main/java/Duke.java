3a7e3b9ae0473c6030ac4842118426443c8f3268
Ruiting <ruiting245831717@gmail.com>
2019-09-24 15:38:53 +0800

    level 3 Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,25 +16,54 @@ public class Duke {
         System.out.println("Hello! I am Duke\nWhat can I do for you?");
         System.out.println(separateLine);
 
+
+//        String line="";
+//        //String[] list=new String[100];
+//        List<String> list = new ArrayList<String>();
+//        Scanner in =new Scanner(System.in);
+//        while(!( line = in.nextLine()).equals("bye")) {
+//            if(!line.equals("list")) {
+//                System.out.println(separateLine + "\n" +"Added: "+ line + "\n" + separateLine);
+//                list.add(line);
+//            }
+//            else
+//            {
+//                System.out.println(separateLine);
+//                //System.out.println(list.toString());
+//                //Stream.of(list.toString()).forEach(System.out::println);
+//                for(int i=1;i<=list.size();i++)
+//                {
+//                    System.out.println(i+". "+list.get(i-1));
+//                }
+//                System.out.println(separateLine);
+//            }
+//        }
+
+
         String line="";
-        //String[] list=new String[100];
-        List<String> list = new ArrayList<String>();
+        List<Task> list = new ArrayList<Task>();
         Scanner in =new Scanner(System.in);
         while(!( line = in.nextLine()).equals("bye")) {
-            if(!line.equals("list")) {
-                System.out.println(separateLine + "\n" +"Added: "+ line + "\n" + separateLine);
-                list.add(line);
-            }
-            else
-            {
-                System.out.println(separateLine);
+            if(line.equals("list")) {
+                System.out.println(separateLine+"\n"+"Here are the tasks in your list:");
                 //System.out.println(list.toString());
                 //Stream.of(list.toString()).forEach(System.out::println);
                 for(int i=1;i<=list.size();i++)
                 {
-                    System.out.println(i+". "+list.get(i-1));
+                    System.out.println(i+". "+"["+list.get(i-1).getStatusIcon()+"] "+list.get(i-1).getDescription());
                 }
                 System.out.println(separateLine);
+            }
+            else if (line.contains("done"))
+            {
+                int idx=Integer.parseInt(line.substring(5));
+                list.get(idx-1).markAsDone();
+                System.out.println(separateLine + "\n" +"Nice! I've marked this task as done");
+                System.out.println("["+list.get(idx-1).getStatusIcon()+"] "+list.get(idx-1).getDescription());
+            }
+            else{
+                System.out.println(separateLine + "\n" +"Added: "+ line + "\n" + separateLine);
+                list.add(new Task(line));
             }
         }
         if (line.equals("bye")) {
