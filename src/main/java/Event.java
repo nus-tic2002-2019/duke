06e687eb275347367c60c6150ecc0ea1036e3927
lim <e0374012@u.nus.edu>
2019-09-25 22:03:14 +0800

    level-4 event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,17 @@
+public class Event extends Task {
+
+    protected boolean isEvent;
+    protected String at;
+
+    public Event(String description, String at) {
+        super(description);
+        this.at = at;
+        isEvent = false;
+    }
+
+    @Override
+    public String toString(){
+        return "[E]" + super.toString() +  "(at:" + at  + ")" ;
+    }
+
+}
