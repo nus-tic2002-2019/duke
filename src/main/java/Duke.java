976e2e7d239ceb76df129338434a91e976b2c65a
zyjarvis <zyjarvis@gmail.com>
2019-09-28 17:25:31 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,15 +13,17 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         split_line();
+        String[] tasks = new String[100];
+        int size = 0;
 
-        chatting();
+        chatting(tasks, size);
     }
 
     public static void split_line() {
         System.out.println("--------------------------------------------------------");
     }
 
-    public static void chatting() {
+    public static void chatting(String[] tasks, int size) {
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
@@ -32,10 +34,19 @@ public class Duke {
             split_line();
             return;
         }
-        else {
-            System.out.println(line);
+        else if(line.toLowerCase().equals ("list")) {
+            for (int i = 0; i < size;  i++) {
+                System.out.println(i+1 + ". " + tasks[i]);
+            }
             split_line();
-            chatting();
+            chatting(tasks, size);
+        }
+        else {
+            System.out.println("added: " + line);
+            split_line();
+            tasks[size] = line;
+            size++;
+            chatting(tasks, size);
         }
     }
 }
