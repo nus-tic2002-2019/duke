3cfcb44afe9b6a8e3e4cdb4b215afbacf85ce86a
ahjimomo <kokwoon.ng@gmail.com>
2019-09-29 18:06:09 +0800

    Class Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class Task {
+    protected String task;
+    protected Boolean done;
+
+    //Constructor
+    public Task(String input)
+    {
+        this.task = input;
+        this.done = false;
+    }
+
+    //Accessor
+    public String printTask()
+    {
+        return this.task;
+    }
+
+    public Boolean isDone()
+    {
+        return this.done;
+    }
+
+    public String getIcon() {
+        return (done ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    //Modifier
+    public void markDone()
+    {
+        this.done = true;
+    }
+
 }
