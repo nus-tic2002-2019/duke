a8dfcc3d2ccefa63c31dfe039df271f7785c783d
IcyNoPeople <leowjunhui2@hotmail.com>
2019-09-29 15:05:04 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static String[] storeWords = new String[100];
-    private static int storeWordCount = 0;
+    private static Task[] Tasks = new Task[100];
+    private static int numberOfTask = 0;
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -15,6 +15,10 @@ public class Duke {
         Echo();
 
     }
+    public static void addTask(Task s){
+        Tasks[numberOfTask] = s;
+        numberOfTask++;
+    }
 
     public static void Echo() {
         String line;
@@ -25,18 +29,65 @@ public class Duke {
         while(byebye) {
             System.out.print("Type something: ");
             line = in.nextLine();
+            String itemName = " ";
+            int dividerPosition =0;
+            if(line.contains(" ")){
+            dividerPosition = line.indexOf(" ");
+            itemName = line.substring(0, dividerPosition);
+            }
+
+            /* switch (itemName) {
+                case "bye":  monthString = "January";
+                    break;
+                case "list":  monthString = "February";
+                    break;
+                case 3:  monthString = "March";
+                    break;
+                case 4:  monthString = "April";
+                    break;
+                case 5:  monthString = "May";
+                    break;
+                case 6:  monthString = "June";
+                    break;
+                case 7:  monthString = "July";
+                    break;
+                case 8:  monthString = "August";
+                    break;
+                case 9:  monthString = "September";
+                    break;
+                case 10: monthString = "October";
+                    break;
+                case 11: monthString = "November";
+                    break;
+                case 12: monthString = "December";
+                    break;
+                default: monthString = "Invalid month";
+                    break;
+            }*/
+
             if (s1.equalsIgnoreCase(line)) {
                 byebye = false;
             }
             else if(line.equalsIgnoreCase("list")){
-                for(int i = 0; i<storeWordCount; i++){
-                    System.out.println((i+1) + ". "+ storeWords[i]);
+                for(int i = 0; i< numberOfTask; i++){
+                    System.out.println((i+1) + ". "+ " [" + Tasks[i].getStatusIcon() + "] "+Tasks[i].getTask());
                 }
             }
+            else if(itemName.equalsIgnoreCase("done")){
+                String taskNumber = line.substring(dividerPosition+1,line.length());
+                int taskIndex = Integer.parseInt(taskNumber)-1;
+                if(numberOfTask >=taskIndex + 1 && taskIndex >= 0) {
+                    Tasks[taskIndex].edit_done(true);
+                    System.out.println("Nice! I've marked this task as done: \n" +
+                            " [" + Tasks[taskIndex].getStatusIcon() + "] " + Tasks[taskIndex].getTask());
+                }
+                else{
+                    System.out.println("Not such task");
+                }
+            }
+            
             else{
-                storeWords[storeWordCount] = line;
-                storeWordCount++;
-
+                addTask(new Task(line));
                 System.out.println("added: "+ line);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+    protected String storeWords;
+    protected boolean done;
+
+
+    public Task(String thingsToDo){
+        this.storeWords = thingsToDo;
+        this.done = false;
+    }
+
+    public String getTask(){
+        return storeWords;
+    }
+    public boolean isDone(){
+        return done;
+    }
+    public void edit_done(boolean is_it_done){
+        done = is_it_done;
+    }
+    public void edit_task(String thingsToDo){
+        storeWords = thingsToDo;
+    }
+    public String getStatusIcon() {
+        return (done ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+
+}
