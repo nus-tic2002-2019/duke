63691e82aa934b5f43c4517bc158eeab2341eaf7
wan yin <wanyin133484@gmail.com>
2019-10-01 19:59:04 +0800

    Revert "Completed Level-5"
    This reverts commit 898654ee1163a5de8d527b5bef87a5aa492f9483.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,21 @@
 import java.util.Scanner;
 
 public class Duke {
-    //private static Task[] tasklist = new Task[100];
-    private static ArrayList<Task> tasklist=new ArrayList<Task>();
+    private static Task[] tasklist = new Task[100];
     private static int taskCount = 0;
 
     public static void addTask(Task s){
-        tasklist.add(taskCount,s);
+        tasklist[taskCount] = s;
         taskCount++;
     }
-    public static void deleteTask(int index){
-        taskCount--;
-        tasklist.remove(index);
-    }
+    //public static void printList(){
+    //  for ( int i = 0; i<taskCount;i++){
+    //    System.out.println(tasklist[i].toString());
+    //  }
+    //}
     public static void printLine(){
             System.out.println("_______________________________");
     }
-    public static void checkException(String message){
-    try{
-     throw new DukeException(message);
-      }
-      catch (DukeException ex){
-              System.out.println(ex.getMessage());
-        }
-        printLine();
-    }
-    
     public static void main(String[] args){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -37,7 +27,9 @@ public class Duke {
         System.out.println("What can I do for you?");
         Scanner myObj = new Scanner(System.in);
         String userInput;
-        //int index = 0;
+        int index = 0;
+        try {
+
             while (myObj.hasNextLine()) {
                 userInput = myObj.nextLine();
                 String[] split = userInput.split(" ");
@@ -45,91 +37,60 @@ public class Duke {
                 if (userInput.equals("list")) {
                     printLine();
                     System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ". " + tasklist.get(i).toString());
+                    for (int i = 0; i < index; i++) {
+                        System.out.println((i + 1) + ". " + tasklist[i].toString());
                     }
                     printLine();
                     continue;
                 }
                 if (userInput.equals("bye")) {
                     break;
-                }
-                if (first.equals("delete")) {
+                } else if (first.equals("done")) {
                     String second = split[1];
-                    printLine();
-                    System.out.println("Noted. I've removed this task: ");
-                    System.out.println(tasklist.get(Integer.parseInt(second) - 1).toString());
-                    System.out.println("Now you have " + (taskCount-1) + " tasks in the list.");
-                    printLine();
-                    deleteTask(Integer.parseInt(second) - 1);
-                }
-                 else if (first.equals("done")) {
-                    String second = split[1];
-                    tasklist.get(Integer.parseInt(second) - 1).markAsDone();
+                    tasklist[Integer.parseInt(second) - 1].markAsDone();
                     printLine();
                     System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println(tasklist.get(Integer.parseInt(second) - 1).getStatusIcon() + " " + tasklist.get(Integer.parseInt(second) - 1).description);
+                    System.out.println(tasklist[Integer.parseInt(second) - 1].getStatusIcon() + " " + tasklist[Integer.parseInt(second) - 1].description);
                     printLine();
                 } else {
                     printLine();
+                    System.out.println("Got it. I've added this task: ");
                     if (first.equals("todo")) {
-                        try{
                         String[] split01 = userInput.split(" ");
                         String firstTD = split01[0];
                         String secondTD = split01[1];
                         addTask(new ToDo(secondTD));
-                        System.out.println("Got it. I've added this task: ");
-                        System.out.println(tasklist.get(taskCount-1).toString());
-                        System.out.println("Now you have " + taskCount + " tasks in the list.");
-                        }
-                        catch (ArrayIndexOutOfBoundsException e){
-                          checkException("☹ OOPS!!! The description of a todo cannot be empty.");
-                          continue;
-                        }
+                        System.out.println(tasklist[index].toString());
+                        index++;
                     }
                     if (first.equals("deadline")) {
-                      try{
-                        String[] headerSplit02 = userInput.split("deadline");
-                        String[] split02 = headerSplit02[1].split("/by");
+                        String[] split02 = userInput.split("/by");
                         String firstD = split02[0];
                         String secondD = split02[1];
                         addTask(new Deadline(firstD, secondD));
-                        System.out.println("Got it. I've added this task: ");
-                        System.out.println(tasklist.get(taskCount-1).toString());
-                        System.out.println("Now you have " + taskCount + " tasks in the list.");
-                      }
-                        catch (ArrayIndexOutOfBoundsException e){
-                          checkException("☹ OOPS!!! The description of a deadline cannot be empty.");
-                          continue;
-                        }
+                        System.out.println(tasklist[index].toString());
+                        index++;
                     }
                     if (first.equals("event")) {
-                      try{
-                        String[] headerSplit03 = userInput.split("event");
-                        String[] split03 = headerSplit03[1].split("/at");
+                        String[] split03 = userInput.split("/at");
                         String firstE = split03[0];
                         String secondE = split03[1];
                         addTask(new Event(firstE, secondE));
-                        System.out.println("Got it. I've added this task: ");
-                        System.out.println(tasklist.get(taskCount-1).toString());
-                        System.out.println("Now you have " + taskCount + " tasks in the list.");
-                      }
-                        catch (ArrayIndexOutOfBoundsException e){
-                          checkException("☹ OOPS!!! The description of a event cannot be empty.");
-                          continue;
-                        }
+                        System.out.println(tasklist[index].toString());
+                        index++;
                     }
-                    else if (!first.equals("event") && !first.equals("deadline") &&!first.equals("todo")) {
-                      checkException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                      continue;
-                    }
+                    System.out.println("Now you have " + index + " tasks in the list.");
                     printLine();
-                    
                 }
-            
+            }
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("OOPS!!! The description of a todo cannot be empty.");
         }
         printLine();
         System.out.println("Bye. Hope to see you again soon!");
         printLine();
     }
 }
+
+
