c69bdba7928365aafc611c5df085244cac9db421
Casopher <43214601+Casopher@users.noreply.github.com>
2019-10-01 18:59:59 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
-public class Duke {
+package duke;
+
+import java.util.Scanner;
+
+public class Duke{
+    
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,25 +11,36 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-                System.out.println("Hello from\n" + logo);
         welcome();
-        
         String line = "";
         Scanner in = new Scanner(System.in);
-        String[] list = new String [100];
+        Todo[] Tasks = new Todo[100];
         int index = 0;
         
         while ( true ){
             line = in.nextLine();
+            if (line.contains(" ")){
+                String[] words = line.split(" ",0);
+                if ( words[0].equals("done")){
+                    int x = Integer.parseInt(words[1]);
+                    if ( x <= index ) {
+                        Tasks[x-1].markAsDone();
+                        response("Nice! I've marked this task as done:\n\t\t" + printTask(Tasks[x-1]));
+                        continue;
+                    }
+                }
+            }
             if ( line.toLowerCase().equals("bye") ){
                 exit();
                 break;
             }
+
             if ( line.toLowerCase().equals("list")){
-                printList(list);
+                printTasks(Tasks);
             } else {  
                 if ( index < 100 ){
-                    list[index]=line;
+                    Todo t = new Todo(line);
+                    Tasks[index] = t;
                     index += 1;
                     response("added:" + line);
                 } else {
@@ -35,10 +51,10 @@ public class Duke {
         }
     }
     
-    public static void printList(String list[]){
+    public static void printTasks(Todo Tasks[]){
         printLine();
-        for ( int i = 0; list[i] != null; i++ ){
-            System.out.printf("\t%d. %s\n",i+1,list[i]);
+        for ( int i = 0; Tasks[i] != null; i++ ){
+            System.out.printf("\t%d. %s\n",i+1,printTask(Tasks[i]));
         }
         printLine();
     }
@@ -61,5 +77,8 @@ public class Duke {
         System.out.printf("\t%s\n",line);
         printLine();
     }
+    
+    public static String printTask(Todo t){
+        return("[" + t.getStatusIcon() + "]" + t.getDescription());
+    }
 }
-
