63898a83fceed6027f721e52b0d28da43c829f03
eunice.kwang <eunice.kwang@accenture.com>
2019-10-02 14:37:58 +0800

    update task 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,28 +37,36 @@ public class Main {
                     System.out.println("[" + list[taskNo - 1].getStatusIcon() + "] " + list[taskNo - 1].getDescription());
                 } else if (function.equalsIgnoreCase("todo")) {
                     if (input.length() > 5) {
-                        //list[counter] = new Todo(input.substring(5));
+                        list[counter] = new Todo(input.substring(5));
                         System.out.println("Got it. I've added this task: ");
                         System.out.println(list[counter].toString());
                         counter++;
-                        System.out.println("Now you have todo borrow book" + counter + " tasks in the list.");
+                        System.out.println("Now you have " + counter + " tasks in the list.");
                     } else {
-                        throw new DukeException(" OOPS!!! The description of a todo cannot be empty.");
+                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
                     }
                 } else if (function.equalsIgnoreCase("deadline")) {
-                    //list[counter] = new Deadline(input.substring(9, input.indexOf("/") - 1), input.substring(input.indexOf("/") + 3));
-                    System.out.println("Got it. I've added this task: ");
-                    System.out.println(list[counter].toString());
-                    counter++;
-                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    if (input.length() > 9) {
+                        list[counter] = new Deadline(input.substring(9, input.indexOf("/") - 1), input.substring(input.indexOf("/") + 3));
+                        System.out.println("Got it. I've added this task: ");
+                        System.out.println(list[counter].toString());
+                        counter++;
+                        System.out.println("Now you have " + counter + " tasks in the list.");
+                    } else {
+                        throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                    }
                 } else if (function.equalsIgnoreCase("event")) {
-                    //list[counter] = new Event(input.substring(6, input.indexOf("/") - 1), input.substring(input.indexOf("/") + 3));
-                    System.out.println("Got it. I've added this task: ");
-                    System.out.println(list[counter].toString());
-                    counter++;
-                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    if (input.length() > 6) {
+                        list[counter] = new Event(input.substring(6, input.indexOf("/") - 1), input.substring(input.indexOf("/") + 3));
+                        System.out.println("Got it. I've added this task: ");
+                        System.out.println(list[counter].toString());
+                        counter++;
+                        System.out.println("Now you have " + counter + " tasks in the list.");
+                    } else {
+                        throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
+                    }
                 } else {
-                    throw newtodo DukeException(" OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
             }
             catch (DukeException e) {
