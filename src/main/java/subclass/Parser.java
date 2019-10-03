f173343d6c3fef5c83ff4795225ee830e1ea1140
Marcus Ng <e0260198@u.nus.edu>
2019-10-03 15:59:25 +0800

    A-moreOOP

--- src/main/java/subclass/Parser.java
+++ src/main/java/subclass/Parser.java
@@ -3,23 +3,19 @@ package subclass;
 public class Parser {
 
     public static String parseTask(String line) {
-        String input = line.substring(line.indexOf(" ") + 1);
-        return input;
+        return line.substring(line.indexOf(" ") + 1);
     }
 
     public static String parseDeadline_by(String line) {
-        String by = line.substring(line.indexOf("/by") + 4);
-        return by;
+        return line.substring(line.indexOf("/by") + 4);
     }
 
     public static String parseEvent_at(String line) {
-        String at = line.substring(line.indexOf("/at") + 4);
-        return at;
+        return line.substring(line.indexOf("/at") + 4);
     }
 
     public static String parseTask_description(String input_items) {
-        String sub_description = input_items.substring(0, input_items.indexOf("/")-1);
-        return sub_description;
+        return input_items.substring(0, input_items.indexOf("/")-1);
     }
 
 }
