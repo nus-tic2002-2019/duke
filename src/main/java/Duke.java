9e112ce2583e1d73962ce5dcb4b2e5e608d3e6f8
Lai Ping <chilaiping@icloud.com>
2019-10-05 15:23:38 +0800

    Level-5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,7 +15,6 @@ public class Duke {
         String line;
         line = in.nextLine();
 
-        // Level 2
         Task[] list = new Task[100];
         int count = 0;
         while (!line.equals("bye")) {
@@ -27,44 +26,57 @@ public class Duke {
                  System.out.println(horizontal_line);
              }
              else if ((line.substring(0,4)).equals("done")){
-                 int position = Integer.parseInt(line.substring(5));
-                 list[position - 1].setDone();
-                 System.out.println( horizontal_line + "Nice! I've marked this task as done: " );
-                 System.out.println('[' + list[position -1].getStatusIcon() + "] " +
-                         list[position -1].description + '\n' + horizontal_line);
+                 try {
+                     int position = Integer.parseInt(line.substring(5));
+                     list[position - 1].setDone();
+                     System.out.println(horizontal_line + "Nice! I've marked this task as done: ");
+                     System.out.println('[' + list[position - 1].getStatusIcon() + "] " +
+                             list[position - 1].description + '\n' + horizontal_line);
+                 } catch (IndexOutOfBoundsException e) {
+                     System.out.println( "☹ OOPS!!! The description of a done cannot be empty.");
+                 }
              }
              else if (line.contains("todo")) {
-                 list[count] = new Todo (line.substring(5));
-                 System.out.println(horizontal_line + "Got it. I've added this task: ");
-                 System.out.println(" " + list[count].toString());
-                 System.out.println("Now you have " + (count + 1) + " tasks in the list.\n" + horizontal_line);
-                 count ++;
+                 try {
+                     list[count] = new Todo(line.substring(5));
+                     System.out.println(horizontal_line + "Got it. I've added this task: ");
+                     System.out.println(" " + list[count].toString());
+                     System.out.println("Now you have " + (count + 1) + " tasks in the list.\n" + horizontal_line);
+                     count++;
+                 } catch (IndexOutOfBoundsException e) {
+                     System.out.println( "☹ OOPS!!! The description of a todo cannot be empty.");
+                 }
              }
              else if (line.contains("deadline")){
-                 int position = line.indexOf("/");
-                 list[count] = new Deadlines(line.substring(9,position-1),line.substring(position + 4));
-                 System.out.println(horizontal_line + "Got it. I've added this task: ");
-                 System.out.println(" " + list[count].toString());
-                 System.out.println("Now you have " + (count + 1) + " tasks in the list.\n" + horizontal_line);
-                 count++;
+                 try {
+                     int position = line.indexOf("/");
+                     list[count] = new Deadlines(line.substring(9, position - 1), line.substring(position + 4));
+                     System.out.println(horizontal_line + "Got it. I've added this task: ");
+                     System.out.println(" " + list[count].toString());
+                     System.out.println("Now you have " + (count + 1) + " tasks in the list.\n" + horizontal_line);
+                     count++;
+                 } catch (IndexOutOfBoundsException e) {
+                     System.out.println( "☹ OOPS!!! The description of a deadline cannot be empty.");
+                 }
 
              } else if (line.contains("event")) {
-                 int position_slash = line.indexOf("/");
-                 int position_time = line.indexOf(" " , position_slash);
-                 list[count] = new Event(line.substring(6,position_slash-1),line.substring(position_slash + 4));
-                 System.out.println(horizontal_line + "Got it. I've added this task: ");
-                 System.out.println(" " + list[count].toString());
-                 System.out.println("Now you have " + (count + 1) + " tasks in the list.\n" + horizontal_line);
-                 count++;
+                 try {
+                     int position_slash = line.indexOf("/");
+                     int position_time = line.indexOf(" ", position_slash);
+                     list[count] = new Event(line.substring(6, position_slash - 1), line.substring(position_slash + 4));
+                     System.out.println(horizontal_line + "Got it. I've added this task: ");
+                     System.out.println(" " + list[count].toString());
+                     System.out.println("Now you have " + (count + 1) + " tasks in the list.\n" + horizontal_line);
+                     count++;
+                 } catch (IndexOutOfBoundsException e) {
+                     System.out.println( "☹ OOPS!!! The description of a event cannot be empty.");
+                 }
              } else
              {
-                 list[count] = new Task (line);//add_task;
-                 System.out.println(horizontal_line + "Added: " + list[count].description + '\n' + horizontal_line);
-                 count++;
+                 System.out.println(horizontal_line + "\n☹ OOPS!!! I'm sorry, but I don't know what that means :-( \n" + horizontal_line);
              }
              line = in.nextLine();
         } System.out.println(horizontal_line + "Bye. Hope to see you again soon!" +'\n'+ horizontal_line);
     }
 }
 
-//public class Task {
