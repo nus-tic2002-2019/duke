bc712e00181ebf2dee07e180e51888d0d2726688
Nuri Irfani Masri <nuriirfani@outlook.com>
2019-10-06 14:56:53 +0800

    Update DukeChildException.java
    made exception methods public

--- src/main/java/DukeChildException.java
+++ src/main/java/DukeChildException.java
@@ -5,34 +5,38 @@ public class DukeChildException {
 
         private static final long serialVersionUID = 7845420361544789423L;
         public nullDescription(String message) {
+
             super(message);
         }
     }
 
     //invalid command
-    private static class invalidInput extends DukeBaseException {
+    public static class invalidInput extends DukeBaseException {
 
 
         private static final long serialVersionUID = -902479303575155928L;
         public invalidInput(String message) {
+
             super(message);
         }
     }
 
     //deadline entered without date
-    private static class deadDate extends DukeBaseException {
+    public static class deadDate extends DukeBaseException {
 
         private static final long serialVersionUID = 3459056459186851874L;
         public deadDate(String message) {
+
             super(message);
         }
     }
 
     //event entered without time
-    private static class eventTime extends DukeBaseException {
+    public static class eventTime extends DukeBaseException {
 
         private static final long serialVersionUID = -8995742482064551983L;
         public eventTime(String message) {
+
             super(message);
         }
     }
