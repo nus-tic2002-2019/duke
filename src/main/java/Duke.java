31979b4908e5b368addc0efb2444ba052fb7dc91
Nuri Irfani Masri <nuriirfani@outlook.com>
2019-10-06 18:05:12 +0800

    Update Duke.java
    Added more exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,10 +23,15 @@ public class Duke {
             String inputCommand = input.split(" ")[0];
             switch (inputCommand) {
                 case "bye":
+                    System.out.println("Bye! I better see you again soon!");
                     continueInput = false;
                     break;
 
                 case "help":
+                    /*
+                        Lists available commands for user.
+                        Reason for implementing this: just in case future work interviews request to see this.
+                     */
                     System.out.println("[1] todo <task>: Enter a task.");
                     System.out.println("[2] deadline <task> /by <date>: Enter a task with deadline.");
                     System.out.println("[3] event <task> /at <time or location>: Enter a task with date or location.");
@@ -35,22 +40,32 @@ public class Duke {
                     System.out.println("[6] bye: Exit application");
                     break;
 
-                //case "bye":
-
                 case "list":
+                    System.out.println("__________________________________________________________________");
                     System.out.println("Your current List of tasks:");
                     for (int i = 0; i < userList.size(); i++) {
-                        System.out.println(userList.get(i).toString());
+                        System.out.println( (i+1) + ". " + userList.get(i).toString());
                     }
+                    System.out.println("__________________________________________________________________");
                     break;
 
                 case "done":
-                    String[] split = input.split(" ");
-                    int doneNum = Integer.parseInt(split[1]); //storing list number
-                    //int doneNum = Integer.parseInt(input.substring(4));
-                    task = userList.get(doneNum - 1);
-                    task.markAsDone();
-                    System.out.println("Done!");
+                    try {
+                        if (input.equals("done")) {
+                            throw new DukeChildException.invalidInput("Task number required!");
+                        }
+                        String[] split = input.split(" ");
+                        int doneNum = Integer.parseInt(split[1]); //storing list number
+                        //int doneNum = Integer.parseInt(input.substring(4));
+                        task = userList.get(doneNum - 1);
+                        task.markAsDone();
+
+                        System.out.println("__________________________________________________________________");
+                        System.out.println("Done!");
+                        System.out.println("__________________________________________________________________");
+                    } catch (DukeChildException.invalidInput e) {
+                        e.printStackTrace();
+                    }
                     break;
 
                 case "todo":
@@ -61,7 +76,9 @@ public class Duke {
                         String todo = input.substring(4);
                         Todo todoTask = new Todo(todo);
                         userList.add(todoTask);
+                        System.out.println("__________________________________________________________________");
                         System.out.println("Okie dokie! Task added: " + System.lineSeparator() + todoTask.toString());
+                        System.out.println("__________________________________________________________________");
                     } catch (DukeChildException.nullDescription e) {
                         e.printStackTrace();
                     }
@@ -93,7 +110,9 @@ public class Duke {
                         Deadline deadline = new Deadline(deadTask, deadBy);
                         userList.add(deadline);
 
+                        System.out.println("__________________________________________________________________");
                         System.out.println("Procrastination is forbidden. Deadline added: " + System.lineSeparator() + deadline.toString());
+                        System.out.println("__________________________________________________________________");
 
                     } catch (DukeChildException.nullDescription e) {
                         e.printStackTrace();
@@ -127,7 +146,9 @@ public class Duke {
                         Event event = new Event(eventTask, eventAt);
                         userList.add(event);
 
+                        System.out.println("__________________________________________________________________");
                         System.out.println("Don't you DARE come late. Event added: " + System.lineSeparator() + event.toString());
+                        System.out.println("__________________________________________________________________");
 
                     } catch (DukeChildException.nullDescription e) {
                         e.printStackTrace();
@@ -147,97 +168,3 @@ public class Duke {
     }
 }
 
-
-
-//////////////////////////////////////OLD CODE////////////////////////////////////////////////////////////////////////
-/*
-
-            if (input.contains("help")) {
-                System.out.println("[1] todo <task>: Enter a task.");
-                System.out.println("[2] deadline <task> /by <date>: Enter a task with deadline.");
-                System.out.println("[3] event <task> /at <time or location>: Enter a task with date or location.");
-                System.out.println("[4] list: Display all tasks.");
-                System.out.println("[5] done <enter task number>: Mark a task as done.");
-                System.out.println("[6] bye: Exit application");
-                input = read.nextLine();
-            }
-
-            if(input.contains("list")) {
-                System.out.println("Your current List of tasks:");
-                for (int i = 0; i < userList.size(); i++) {
-                    System.out.println(userList.get(i).toString());
-                }
-                input = read.nextLine();
-                //System.out.println("List");
-            }
-            if (input.contains("bye")) {
-                System.out.println("Bye");
-                System.exit(0);
-            }
-
-            if (input.contains("done")) {
-                String[] split = input.split(" ");
-                int doneNum = Integer.parseInt(split[1]); //storing list number
-                //int doneNum = Integer.parseInt(input.substring(4));
-                task = userList.get(doneNum - 1);
-                task.markAsDone();
-                System.out.println("Done!");
-                input = read.nextLine();
-            }
-
-            if(input.contains("todo")) {
-                String todo = input.substring(4);
-                Todo todoTask = new Todo(todo);
-                userList.add(todoTask);
-
-                System.out.println("Okie dokie! Task added: " + System.lineSeparator() + todoTask.toString());
-                input = read.nextLine();
-            }
-
-            if (input.contains(("deadline"))) {
-                String[] split = input.split("/");
-                String deadTask = split[0];
-                String deadBy = split[1];
-
-                deadTask = deadTask.substring(9);
-                deadBy = deadBy.substring(3);
-                //System.out.println(deadTask);
-                //System.out.println(deadBy);
-
-                Deadline deadline = new Deadline(deadTask, deadBy);
-                userList.add(deadline);
-
-                System.out.println("Procrastination is forbidden. Deadline added: " + System.lineSeparator() + deadline.toString());
-                input = read.nextLine();
-            }
-
-            if (input.contains("event")) {
-                String[] split = input.split("/");
-                String eventTask = split[0];
-                String eventAt = split[1];
-
-                eventTask = eventTask.substring(6);
-                eventAt = eventAt.substring(3);
-                //System.out.println(deadTask);
-                //System.out.println(deadBy);
-
-                Event event = new Event(eventTask, eventAt);
-                userList.add(event);
-
-                System.out.println("Don't you DARE come late. Event added: " + System.lineSeparator() + event.toString());
-                input = read.nextLine();
-            }
-
-            if ((!input.contains("list")) &&
-                    (!input.contains("bye")) &&
-                    (!input.contains("done")) &&
-                    (!input.contains("deadline")) &&
-                    (!input.contains("event")) &&
-                    (!input.contains("help"))) {
-                task = new Task(input);
-                userList.add(task);
-                System.out.println("added:" + input);
-                input = read.nextLine();
-            }
-
-            */
