f040891f4a97a123167aa24ebd9c7849b72238a2
kaydenkhor <k-denkhor@live.com>
2019-10-07 21:43:35 +0800

    Level-6 Delete function implemented, Changed all Array task into collection class as ArrayList<Task> history, Removed count variable. Done by E0373956, Khor KY

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,34 +1,34 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class Duke {
     private static void DukeReply(String replies) { // DukeReplying format, only changing variable is replies/ input from main function.
         System.out.println("\t________________________________________________________________\n" + "\t" + replies + "\n\t________________________________________________________________\n");
     }
 
-    private static String DukeList(Task[] listing) throws DukeException { // Listing function: go through every string element and concatenate them together to become a huge string return the huge string back to main function.
+    private static String DukeList(ArrayList<Task> history) throws DukeException { // Listing function: go through every string element and concatenate them together to become a huge string return the huge string back to main function.
+        if (history.size() == 0) {
+            throw new DukeException();
+        }
         int index = 1;
         String answer = "";
-        for (int i = 0; i < listing.length; i++) {
-            if (listing[i] == null) {
-                throw new DukeException();
-            }
-            answer = (answer + index + "." + listing[i].getType() + listing[i].getTaskStatus() + " " + listing[i].getTask() + " " + listing[i].getDetails());
+        for (int i = 0; i < history.size(); i++) {
+            answer = (answer + index + "." + history.get(i).getType() + history.get(i).getTaskStatus() + " " + history.get(i).getTask() + " " + history.get(i).getDetails());
             index++;
-            if (listing[i + 1] == null) {
-                break;
+            if (i== history.size()-1){
+                continue;
             }
             answer += "\n\t";
         }
         return answer;
     }
 
-    private static String DukeListDone(Task[] listing, int index) {
-        listing[index - 1].setTaskDone();
-        return listing[index - 1].getType() + listing[index - 1].getTaskStatus() + " " + listing[index - 1].getTask();
+    private static String DukeListDone(ArrayList<Task> listing, int index) {
+        listing.get(index - 1).setTaskDone();
+        return listing.get(index - 1).getType() + listing.get(index - 1).getTaskStatus() + " " + listing.get(index - 1).getTask();
     }
 
-    private static String DukeDone(Task[] history, String input, int count) throws DukeExceptionInvalidTaskNumberDone {
+    private static String DukeDone(ArrayList<Task> history, String input) throws DukeExceptionInvalidTaskNumberDone {
         if (input.length() < 5 || input.charAt(4) != ' ' || input.charAt(5) == ' ') { // Handling errors: When user types done1 done2 or done   2
             throw new DukeExceptionInvalidTaskNumberDone();
         }
@@ -40,31 +40,57 @@ public class Duke {
             return "Please enter a valid number";
         }
         int index = Integer.parseInt(number);
-        if (index > count) { //Handling Errors: When user keys in index which is more than the list it has.
+        if (index > history.size()) { //Handling Errors: When user keys in index which is more than the list it has.
             throw new DukeExceptionInvalidTaskNumberDone();
         } else {
-            return "Nice! I've marked this task as done: \n\t" + DukeListDone(history, index);
+            history.get(index-1).setTaskDone();
+            return "Nice! I've marked this task as done: \n\t" + DukeDisplayTask(history, index-1);
         }
     }
 
-    private static void enterTodo(Task[] history, String input, int count){
-        history[count] = new Todo(input.substring(5));
+    private static String DukeDelete(ArrayList<Task> history, String input) throws DukeExceptionInvalidTaskNumberDone {
+        if (input.length() < 7 || input.charAt(6) != ' ' || input.charAt(7) == ' ') { // Handling errors: When user types done1 done2 or done   2
+            throw new DukeExceptionInvalidTaskNumberDone();
+        }
+        String number = input.substring(7);
+        try {
+            int index = Integer.parseInt(number);
+        }
+        catch (NumberFormatException e) {
+            return "Please enter a valid number";
+        }
+        int index = Integer.parseInt(number);
+        if (index > history.size()) {
+            throw new DukeExceptionInvalidTaskNumberDone();
+        } else {
+            index = index -1;
+            String removed = "Noted. I've removed this task: \n\t\t" + DukeDisplayTask(history, index);
+            history.remove(index);
+            removed = removed + "\n\tNow you have " + history.size() + " tasks in the list.";
+            return removed;
+        }
     }
 
-    private static void enterDeadline(Task[] history, String input, int count){
+    private static void enterTodo(ArrayList<Task> history, String input, int count) {
+        history.add(count, new Todo(input.substring(5)));
+    }
+
+    private static void enterDeadline(ArrayList<Task> history, String input, int count) {
         int idx = input.indexOf('/');
-        history[count] = new Deadlines(input.substring(9, idx - 1), input.substring(idx + 4));
+        history.add(count, new Deadlines(input.substring(9, idx - 1), input.substring(idx + 4)));
     }
 
-    private static void enterEvent(Task[] history, String input, int count) {
+    private static void enterEvent(ArrayList<Task> history, String input, int count) {
         int idx = input.indexOf('/');
-        history[count] = new Events(input.substring(6, idx - 1), input.substring(idx + 4));
+        history.add(count, new Events(input.substring(6, idx - 1), input.substring(idx + 4)));
     }
 
+    private static String DukeDisplayTask(ArrayList<Task> history, int number) {
+        return history.get(number).getType() + history.get(number).getTaskStatus() + " " + history.get(number).getTask() + " " + history.get(number).getDetails();
+    }
 
     public static void main(String[] args) {
-        int count = 0;
-        Task[] history = new Task[100];
+        ArrayList<Task> history = new ArrayList<Task>();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -76,64 +102,72 @@ public class Duke {
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
             input = input.trim();
-                if (input.isEmpty()) { // if input is empty space or just enter, i will ignore it and not add to the list
-                    continue;
+            if (input.isEmpty()) { // if input is empty space or just enter, i will ignore it and not add to the list
+                continue;
+            }
+            if (input.equalsIgnoreCase("bye")) { // if user enters "bye", we will exit the program
+                DukeReply("Bye. Hope to see you again soon!");
+                break;
+            }
+            if (input.toLowerCase().contains("done")) {
+                try {
+                    DukeReply(DukeDone(history, input));
                 }
-                if (input.equalsIgnoreCase("bye")) { // if user enters "bye", we will exit the program
-                    DukeReply("Bye. Hope to see you again soon!");
-                    break;
+                catch (DukeExceptionInvalidTaskNumberDone e) {
+                    DukeReply("You've entered an invalid number of task list, please enter a valid task number again.");
                 }
-                if (input.toLowerCase().contains("done")) {
+                continue;
+            }
+            if (input.equalsIgnoreCase("list")) { // if the user enters "list", it will list out everything, else add to the list. refer number 5.
+                try {
+                    DukeReply("Here are the Tasks in your list:\n\t" + DukeList(history));
+                }
+                catch (DukeException e) {
+                    DukeReply("You have nothing in the list.");
+                }
+                continue;
+            }
+            if (input.toLowerCase().contains("delete")) { // if the user enters "delete", it will activate the delete task function
+                try {
+                    DukeReply(DukeDelete(history, input));
+                }
+                catch (DukeExceptionInvalidTaskNumberDone e) {
+                    DukeReply("You've entered an invalid number of task list, please enter a valid task number again.");
+                }
+                continue;
+            }
+            // add the other words into the list
+            else {
+                if (input.toLowerCase().contains("todo")) {
                     try {
-                        DukeReply(DukeDone(history, input, count));
+                        enterTodo(history, input, history.size());
                     }
-                    catch (DukeExceptionInvalidTaskNumberDone e){
-                        DukeReply("You've entered a value more than the number of task list, please enter a valid task number again.");
-                    }
-                    continue;
-                }
-                if (input.equalsIgnoreCase("list")) { // if the user enters "list", it will list out everything, else add to the list. refer number 5.
-                    try {
-                        input = "Here are the Tasks in your list:\n\t" + DukeList(history);
-                    }
-                    catch (DukeException e) {
-                        input = "You have nothing in the list.";
-                    }
-                }
-                // add the other words into the list
-                else {
-                    if (input.toLowerCase().contains("todo")) {
-                        try {
-                            enterTodo(history, input, count);
-                        }
-                        catch (StringIndexOutOfBoundsException e){
-                            DukeReply("\u2639 OOPS!!! The description of a todo cannot be empty.");
-                            continue;
-                        }
-                    } else if (input.toLowerCase().contains("deadline")) {
-                        try {
-                            enterDeadline(history, input, count);
-                        }
-                        catch (StringIndexOutOfBoundsException e){
-                            DukeReply("\u2639 OOPS!!! The description of a deadline cannot be empty.");
-                            continue;
-                        }
-                    } else if (input.toLowerCase().contains("event")) {
-                        try {
-                            enterEvent(history, input, count);
-                        }
-                        catch (StringIndexOutOfBoundsException e){
-                            DukeReply("\u2639 OOPS!!! The description of a event cannot be empty.");
-                            continue;
-                        }
-                    } else {
-                        DukeReply("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    catch (StringIndexOutOfBoundsException e) {
+                        DukeReply("\u2639 OOPS!!! The description of a todo cannot be empty.");
                         continue;
                     }
-                    input = "Got it. I've added this task:\n\t\t" + history[count].getType() + history[count].getTaskStatus() + " " + history[count].getTask() + " " + history[count].getDetails() + "\n\tNow you have " + (count + 1) + " tasks in the list.";
-                    count++;
+                } else if (input.toLowerCase().contains("deadline")) {
+                    try {
+                        enterDeadline(history, input, history.size());
+                    }
+                    catch (StringIndexOutOfBoundsException e) {
+                        DukeReply("\u2639 OOPS!!! The description of a deadline cannot be empty.");
+                        continue;
+                    }
+                } else if (input.toLowerCase().contains("event")) {
+                    try {
+                        enterEvent(history, input, history.size());
+                    }
+                    catch (StringIndexOutOfBoundsException e) {
+                        DukeReply("\u2639 OOPS!!! The description of a event cannot be empty.");
+                        continue;
+                    }
+                } else {
+                    DukeReply("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    continue;
                 }
-            DukeReply(input);
+                DukeReply("Got it. I've added this task:\n\t\t" + DukeDisplayTask(history, history.size() - 1) + "\n\tNow you have " + history.size() + " tasks in the list.");
+            }
         }
     }
 }
