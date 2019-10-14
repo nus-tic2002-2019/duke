097d2e074db9fb9f46b1a65032a9c797808d1546
Casopher <43214601+Casopher@users.noreply.github.com>
2019-10-15 00:46:07 +0800

    Update Todo.java

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,22 +1,12 @@
 package duke;
 
 public class Todo extends Task{
-  private boolean isDone;
   
   public Todo (String description){
     super(description);
-    this.isDone = false;
   }
-  
-  public void markAsDone (){
-    this.isDone = true;
-  }
-  
-  public boolean isDone (){
-    return this.isDone;
-  }
-  
-  public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
-  }
+
+  public String printTask(){
+      return ("[T][" + this.getStatusIcon() + "] " + this.description );
+    }
 }
