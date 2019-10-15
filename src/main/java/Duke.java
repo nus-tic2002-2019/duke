7a7ba78dfab84163cdc5de95bd08ce6607b6c51f
cheralynwsw <e0374087@u.nus.edu>
2019-10-15 19:58:56 +0800

    Level 1 Complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,7 +7,32 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println("Hello I'm Duke");
+        System.out.println("What can I do for you?");
+
+        boolean cont = true;
+        while(cont) { // level 1 - Echo
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+
+            if (input.toLowerCase().equals ("bye")){
+                System.out.println("Bye. Hope to see you soon!");
+            }
+            else
+                DukeResponse.echo(input);
+        }
+
     }
 }
-//hello
+
+/*int count = 0;
+while (!input.equals("bye")){
+    input = in.nextLine();
+
+    if(input.equals("list")){
+        for (int i=0; i<count; i++){
+            System.out.println(i+1 + "." + line[i]);
+        }
+    }
+}*/
---
+++ src/main/java/DukeResponse.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DukeResponse extends Duke {
+
+    public static void echo(String input) {
+        System.out.println(input);
+    }
+
+   /* public static void add (String input){
+        system.out.println(input);
+    }*/
+
+}
