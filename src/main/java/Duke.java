bb3c8155e8604bcf48912e51eb5fca8ef37a43a4
Natalie Yeo <>
2019-10-28 19:42:22 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,11 +10,20 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        String[] new_list = new String[100];
         String line;
         Scanner in = new Scanner(System.in);
+        int i = 0;
         line = in.nextLine();
         while (!line.equals("bye")){
-            System.out.println(line);
+            new_list[i] = line;
+            System.out.println("added: " + line);
+            if (line.equals("list")){
+                for (int a = 0; a < i; a++){
+                    System.out.println(new_list[a]);
+                }
+            }
+            i++;
             line = in.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
