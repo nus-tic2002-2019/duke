2daa64376128c4fb4d2aae4908981291911091cc
Dax Tan <this.is.dax@gmail.com>
2019-10-28 20:03:32 +0800

    level 5 - Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     private static Task todoList = new Task();
-    private static void printUI() {
-
-    }
+    
     private static void echo() {
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
@@ -45,21 +43,27 @@ public class Duke {
                 case "todo":
                 case "event":
                 case "deadline":
-                    if (choice[0].toLowerCase().equals("todo")) {
-                        todoList.addTodo(line.split(" ", 2)[1]);
-                    } else if (choice[0].toLowerCase().equals("event")) {
-                        todoList.addEvent(line.split(" ", 2)[1]);
-                    } else if (choice[0].toLowerCase().equals("deadline")) {
-                        todoList.addDeadline(line.split(" ", 2)[1]);
+                    try {
+                        if (choice[0].toLowerCase().equals("todo")) {
+                            todoList.addTodo(line.split(" ", 2)[1]);
+                        } else if (choice[0].toLowerCase().equals("event")) {
+                            todoList.addEvent(line.split(" ", 2)[1]);
+                        } else if (choice[0].toLowerCase().equals("deadline")) {
+                            todoList.addDeadline(line.split(" ", 2)[1]);
+                        }
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(line.split(" ", 2)[1]);
+                        System.out.println("Now you have " + todoList.getSize() + " tasks in the list.");
+                        System.out.println("____________________________________________________________");
+                    } catch (Exception e) {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                        System.out.println("____________________________________________________________");
                     }
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(line.split(" ", 2)[1]);
-                    System.out.println("Now you have " + todoList.getSize() + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
                     break;
                 default:
-                    System.out.println("I'm sorry, I didn\'t get that. :(");
+                    System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                     break;
             }
             line = in.nextLine();
@@ -68,52 +72,6 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-//        while(!line.equals("bye")){
-//            // list
-//            if (line.equals("list")) {
-//                int i = 1;
-//                if (todoList.getItemCount() == 0) {
-//                    System.out.println("Your list is empty.");
-//                } else {
-//                    boolean[] status = todoList.getStatus();
-//                    for (String item : todoList.getTask()) {
-//                        if (item != null) {
-//                            System.out.println(i + "." + "[" + (status[i - 1] ? "✓" : "✗") + "] " + item);
-//                            i++;
-//                        }
-//                    }
-//                }
-//            // done
-//            } else if (line.length() >= 4 && line.substring(0, 4).equals("done")) {
-//                try {
-//                    int itemNumber = Integer.parseInt(line.substring(5, line.length())) - 1;
-//                    if (todoList.doTask(itemNumber) && todoList.getItemCount() > itemNumber) {
-//                        // TODO: change task to an array of object class instead?
-//                        String[] taskList = todoList.getTask();
-//                        boolean[] taskStatus = todoList.getStatus();
-//                        System.out.println("Nice! I've marked this task as done:");
-//                        System.out.println("[" + (taskStatus[itemNumber] ? "✓" : "✗") + "] " + taskList[itemNumber]);
-//                    } else {
-//                        System.out.println("Unable to mark task as done.");
-//                    }
-//                } catch (NumberFormatException error) {
-//                    System.out.println("Unable to mark task as done.");
-//                } catch (StringIndexOutOfBoundsException error) {
-//                    System.out.println("Unable to mark task as done.");
-//                } catch (ArrayIndexOutOfBoundsException error) {
-//                    System.out.println("Unable to mark task as done.");
-//                }
-//            // add task
-//            } else {
-//                System.out.println(line);
-//                if (!todoList.setTask(line)) {
-//                    System.out.println("Your task list is full, unable to add more task.");
-//                }
-//            }
-//            line = in.nextLine();
-//        }
-//        System.out.println("Bye. Hope to see you again soon!");
-//    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
