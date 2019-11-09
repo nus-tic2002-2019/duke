d4eb025c0c986c5a3663ba39ea6397077d2ad3ee
Yralle <yralleljg@gmail.com>
2019-11-10 04:41:23 +0800

    level 7 completed

--- src/main/java/main/Storage.java
+++ src/main/java/main/Storage.java
@@ -1,6 +1,11 @@
 package main;
 
+// TODO Create filepath and implement save and Load features
 public class Storage {
 
-    
+    private String filepath;
+
+    public Storage(String filepath) {
+        this.filepath = filepath;
+    }
 }
