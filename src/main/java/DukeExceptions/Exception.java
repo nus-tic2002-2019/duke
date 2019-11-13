24077c031fdb3c2d5acc9e3778324e4369bc38d5
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-13 16:18:45 +0800

    Added Delete exception handling

--- src/main/java/DukeExceptions/Exception.java
+++ src/main/java/DukeExceptions/Exception.java
@@ -2,14 +2,15 @@ package DukeExceptions;
 public class Exception extends RuntimeException {
 
     public enum Code {
-		UNKNOWN_COMMAND,
-		EMPTY_TODO_DESCRIPTION,
+	UNKNOWN_COMMAND,
+	EMPTY_TODO_DESCRIPTION,
         EMPTY_DONE_DESCRIPTION,
         INVALID_TASK_NUMBER,
         EMPTY_DEADLINE_DESCRIPTION,
         MISSING_DEADLINE_DATETIME,
         EMPTY_EVENT_DESCRIPTION,
         MISSING_EVENT_VENUE,
+	EMPTY_DELETE_DESCRIPTION
     }
 
     private Code code;
@@ -21,32 +22,34 @@ public class Exception extends RuntimeException {
     public String errorDescription() {
 
         switch (code) {
-			case UNKNOWN_COMMAND:
-                return "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+		case UNKNOWN_COMMAND:
+			return "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
 				
-			case EMPTY_TODO_DESCRIPTION:
-                return "☹ OOPS!!! The description of a todo cannot be empty.";
+		case EMPTY_TODO_DESCRIPTION:
+                	return "☹ OOPS!!! The description of a todo cannot be empty.";
 				
-            case EMPTY_DONE_DESCRIPTION:
-                return "☹ OOPS!!! You must provide a task number for 'done' command!";
+            	case EMPTY_DONE_DESCRIPTION:
+                	return "☹ OOPS!!! You must provide a task number for 'done' command!";
 				
-            case INVALID_TASK_NUMBER:
-                return "☹ OOPS!!! You entered an invalid task number!";
+            	case INVALID_TASK_NUMBER:
+                	return "☹ OOPS!!! You entered an invalid task number!";
 				
-            case EMPTY_DEADLINE_DESCRIPTION:
-                return "☹ OOPS!!! The description of a deadline cannot be empty.";
+            	case EMPTY_DEADLINE_DESCRIPTION:
+                	return "☹ OOPS!!! The description of a deadline cannot be empty.";
 
-            case MISSING_DEADLINE_DATETIME:
-                return "☹ OOPS!!! The date and or time of a deadline must contain ' /by '.";
+            	case MISSING_DEADLINE_DATETIME:
+                	return "☹ OOPS!!! The date and or time of a deadline must contain ' /by '.";
 
-            case EMPTY_EVENT_DESCRIPTION:
-                return "☹ OOPS!!! The description of an event cannot be empty.";
+            	case EMPTY_EVENT_DESCRIPTION:
+                	return "☹ OOPS!!! The description of an event cannot be empty.";
 
-            case MISSING_EVENT_VENUE:
-                return "☹ OOPS!!! The venue of an event must contain ' /at '.";
-
-            default:
-                break;
+            	case MISSING_EVENT_VENUE:
+                	return "☹ OOPS!!! The venue of an event must contain ' /at '.";
+				
+		case EMPTY_DELETE_DESCRIPTION:
+                	return "☹ OOPS!!! You must provide a task number for 'delete' command!";
+		default:
+                	break;
         }
         return null;
     }
