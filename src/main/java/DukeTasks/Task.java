691918ca35ff6092ad01af1f4eca7f6da4c96c27
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-14 21:54:02 +0800

    removed non public Task classes
    removed non public Task classes
    return correct tick or X symbols

--- src/main/java/DukeTasks/Task.java
+++ src/main/java/DukeTasks/Task.java
@@ -13,7 +13,7 @@ public class Task {
     }
 	
     public String getStatusIcon() {
-        return (isDone ? "[V]" : "[X]"); //return tick or X symbols
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 	public void setStatusIconTrue() {
         this.isDone = true;
@@ -24,46 +24,5 @@ public class Task {
 	@Override
     public String toString() {
         return description;
-    }
-}
-
-// ToDos: tasks without any date/time attached to it e.g., visit new theme park
-class Todo extends Task {
-    protected String by;
-
-    public Todo(String description) {
-        super(description);
-    }
-    @Override
-    public String toString() {
-        return "[T]" + this.getStatusIcon() +" "+ super.toString();
-    }
-}
-// Deadlines: tasks that need to be done before a specific date/time e.g., submit report by 11/10/2019 5pm
-class Deadline extends Task {
-    protected String by;
-
-    public Deadline(String description, String by) {
-        super(description);
-        this.by = by;
-    }
-
-    @Override
-    public String toString() {
-        return "[D]" + this.getStatusIcon() +" "+ super.toString() + " (by: " + by + ")";
-    }
-}
-// Events: tasks that start at a specific time and ends at a specific time e.g., team project meeting on 2/10/2019 2-4pm
-class Events extends Task {
-    protected String at;
-
-    public Events(String description, String at) {
-        super(description);
-        this.at = at;
-    }
-
-    @Override
-    public String toString() {
-        return "[E]" + this.getStatusIcon() +" "+ super.toString() + " (at: " + at + ")";
     }
 }
