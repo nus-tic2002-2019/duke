5bc285a00d9fc2aceaf17b212e276ab892c14f36
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-14 22:36:10 +0800

    Level-7: Save
    Added Storage to save

---
+++ src/main/java/main/Storage.java
@@ -0,0 +1,87 @@
+package main;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+import DukeTasks.*;
+
+public class Storage {
+
+    private Path path = Paths.get(System.getProperty("user.dir"));
+    private File data = new File(path + "/data/duke.txt");
+
+    public Storage() {
+
+    }
+	public ArrayList<Task> readFromFile() {
+
+        ArrayList<Task> toReturn = new ArrayList<>();
+
+        try {
+            Scanner sc = new Scanner(data);
+            while (sc.hasNextLine()) {
+
+                String[] next = sc.nextLine().split("-");
+
+                switch (next[0]) {
+                case ("T"):
+                    Task todo = new Todo(next[2]);
+                    if (Integer.valueOf(next[1]) == 1) {
+                        todo.setStatusIconTrue();
+                    }
+                    toReturn.add(todo);
+                    break;
+                case ("E"):
+                    Task event = new Event(next[2], next[3]);
+                    if (Integer.valueOf(next[1]) == 1) {
+                        event.setStatusIconTrue();
+                    }
+                    toReturn.add(event);
+                    break;
+
+                case ("D"):
+                    Task deadline = new Deadline(next[2], next[3]);
+                    if (Integer.valueOf(next[1]) == 1) {
+                        deadline.setStatusIconTrue();
+                    }
+                    toReturn.add(deadline);
+                    break;
+
+                default:
+                    break;
+                }
+            }
+            sc.close();
+
+        } catch (FileNotFoundException fileExp) {
+            System.out.println(fileExp);
+        }
+        return toReturn;
+    }
+
+
+	public void writeToFile(ArrayList<Task> list) {
+
+        try {
+            File data = new File("duke.txt");
+            FileWriter fw = new FileWriter(data);
+
+            StringBuilder toWrite = new StringBuilder("");
+
+            for (Task task : list) {
+                toWrite.append(task.toString() + " \n");
+            }
+
+            fw.write(toWrite.toString());
+            fw.close();
+
+        } catch (IOException ioExp) {
+            System.err.println(ioExp);
+        }
+    }
+}
