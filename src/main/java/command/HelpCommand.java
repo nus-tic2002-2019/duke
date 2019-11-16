557ac77993aaedc62413e3e14aa04e1647996520
e0260237 <e0260237@u.nus.edu>
2019-11-16 16:10:35 +0800

    Revert back without GUI - got some bugs with GUI.

--- src/main/java/command/HelpCommand.java
+++ src/main/java/command/HelpCommand.java
@@ -24,8 +24,8 @@ public class HelpCommand extends Command{
         System.out.println("This is the list of commands:\n");
         System.out.println("1)bye - To exit program \n");
         System.out.println("2)todo - Input todo task by typing todo <task name>\n");
-        System.out.println("3)deadline - Input deadline task by typing deadline <task name> /<dd-MM-yyyy HH:mm:ss>\n");
-        System.out.println("4)event - Input event task by typing event <task name> /<dd-MM-yyyy HH:mm:ss>\n");
+        System.out.println("3)deadline - Input deadline task by typing deadline <task name> / <dd-MM-yyyy HH:mm:ss>\n");
+        System.out.println("4)event - Input event task by typing event <task name> / <dd-MM-yyyy HH:mm:ss>\n");
         System.out.println("5)delete - Delete task by typing delete + number of the task listed\n");
         System.out.println("6)list - To list out all the existing tasks\n");
         System.out.println("7)done - To add a tick to a completed task\n");
--- src/main/java/duke/launcher.java
+++ src/main/java/duke/launcher.java
@@ -1,13 +1,13 @@
-package duke;
+/*package duke;
 import javafx.application.Application;
 
 /**
  * A launcher class to workaround classpath issues.
  */
-
+/*
 public class launcher{
 
     public static void main(String[] args) {
         Application.launch(Duke.class, args);
     }
-}
+}*/
