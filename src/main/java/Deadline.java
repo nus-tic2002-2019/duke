2ff466fc94e9fed909eef8caf9f03a50744a8b64
Wan Yin Lim <>
2019-11-17 00:08:41 +0800

    Completed Level 8 - Read Date Time

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
     protected String by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime DateTime = LocalDateTime.parse(by,formatter);
+        this.by = DateTime.format(formatter);
     }
     public Deadline(String status,String description, String by) {
         super(description);
-        this.by = by;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime DateTime = LocalDateTime.parse(by,formatter);
+        this.by = DateTime.format(formatter);
         this.setStatus(status);
     }
 
@@ -22,7 +29,9 @@ public class Deadline extends Task {
         return "[D]" + super.toString() + " (by:" + by + ")";
     }
     public String getLast(){
-        this.by = by;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime DateTime = LocalDateTime.parse(by,formatter);
+        this.by = DateTime.format(formatter);
         return by;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
     protected String at;
 
     public Event(String description, String at) {
         super(description);
-        this.at = at;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime DateTime = LocalDateTime.parse(at,formatter);
+        this.at = DateTime.format(formatter);
     }
     public Event(String status, String description, String at) {
         super(description);
-        this.at = at;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime DateTime = LocalDateTime.parse(at,formatter);
+        this.at = DateTime.format(formatter);
         this.setStatus(status);
     }
 
@@ -23,7 +30,9 @@ public class Event extends Task {
     }
 
     public String getLast(){
-        this.at = at;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime DateTime = LocalDateTime.parse(at,formatter);
+        this.at = DateTime.format(formatter);
         return at;
     }
 }
