2523ee514275fa3ac6f94f9b11c750c031c7802b
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-17 22:33:39 +0800

    Added Exception handling

--- src/main/java/DukeCommands/TodoCommand.java
+++ src/main/java/DukeCommands/TodoCommand.java
@@ -1,10 +1,11 @@
-package DukeCommands;
+package newDuke.DukeCommands;
 
-import main.Storage;
-import main.TaskList;
-import main.UI;
-import DukeTasks.*;
+import newDuke.main.Storage;
+import newDuke.main.TaskList;
+import newDuke.main.UI;
+import newDuke.DukeTasks.*;
 import java.util.ArrayList;
+import newDuke.DukeExceptions.Exception;
 
 /**
  * A Command to create an instance of a Todo Task. This will add the Todo Task to the TaskList and the Storage file.
@@ -21,19 +22,27 @@ public class TodoCommand implements Command {
      * Executes the Todo command which creates a Todo Task.
      *
      * <p></p>Taking the TaskList and Storage object of the main Duke class as arguments, this command will create a new
-     * Deadline Task which will then be added to the TaskList and Storage objects. The UI will also be used to print
-     * a newTask message into the console.
+     * Deadline Task which will then be added to the TaskList and Storage objects. The UI will also be used to print a newTask message into the console.
      *
      * @param taskList The TaskList from the main Duke object.
      * @param storage The Storage from the main Duke object.
      */
 
     public String execute(TaskList taskList, Storage storage) {
-
-        ArrayList<Task> list = taskList.getTaskList();
-        Task todo = new Todo(todoName);
-        taskList.addTaskList(todo);
-        storage.writeToFile(list);
-        return UI.newTask(list);
-    }
+		try {
+			try {
+				ArrayList<Task> list = taskList.getTaskList();
+				Task todo = new Todo(todoName);
+				taskList.addTaskList(todo);
+				storage.writeToFile(list);
+				return UI.newTask(list);
+			} catch (ArrayIndexOutOfBoundsException | StringIndexOutOfBoundsException e){
+				if ( todoName==null || todoName.trim().length() == 0){
+					throw new Exception(Exception.Code.EMPTY_TODO_DESCRIPTION);
+				}
+			} catch (Exception e){
+				return e.errorDescription();
+			}
+		}
+	}
 }
