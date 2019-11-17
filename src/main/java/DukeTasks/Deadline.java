06427b1eea97e19a0a246e5b2fcfc630aa5e93f8
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-17 22:39:51 +0800

    added toSave and getDate fxn

--- src/main/java/DukeTasks/Deadline.java
+++ src/main/java/DukeTasks/Deadline.java
@@ -1,16 +1,27 @@
-package DukeTasks; 
+package newDuke.DukeTasks; 
+import java.time.LocalDate;
 
 // Deadlines: tasks that need to be done before a specific date/time e.g., submit report by 11/10/2019 5pm
 public class Deadline extends Task {
     protected String by;
-
+	
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+		this.setSymbol("D");
     }
-
     @Override
     public String toString() {
-        return "[D]" + this.getStatusIcon() +" "+ super.toString() + " (by: " + by + ")";
+        return "["+this.getSymbol()+"]" + this.getStatusIcon() +" "+ super.toString() + " (by: " + by + ")";
     }
+	
+	@Override
+	public String toSave() {
+        return this.getSymbol() + " | " + this.getStatus() + " | " + super.toString() + " | " + by;
+    }
+	@Override
+	public String getDate(){
+		return by;
+	}
+	
 }
