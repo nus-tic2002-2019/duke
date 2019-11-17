31647fe4a5781de74b75a74f6760c060e46dd821
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-17 22:23:29 +0800

    Update DeadlineCommand.java

--- src/main/java/DukeCommands/DeadlineCommand.java
+++ src/main/java/DukeCommands/DeadlineCommand.java
@@ -1,39 +1,64 @@
-package DukeCommands;
+package newDuke.DukeCommands;
 
-import main.Storage;
-import main.TaskList;
-import main.UI;
-import DukeTasks.Task;
-import DukeTasks.Deadline;
-
+import newDuke.main.Storage;
+import newDuke.main.TaskList;
+import newDuke.main.UI;
+import newDuke.DukeTasks.Task;
+import newDuke.DukeTasks.Deadline;
+import newDuke.DukeExceptions.Exception;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 
 /**
  * A Command to create an instance of a Deadline Task. This will add the Deadline Task to the TaskList and the Storage
  * file.
  */
 public class DeadlineCommand implements Command {
+    private String deadlineDetailsWhole;
     private String deadlineName;
-    private String date;
+    private String dateFormatted;
 
-    public DeadlineCommand(String deadlineName, String date) {
-        this.deadlineName = deadlineName;
-        this.date = date;
+    public DeadlineCommand(String deadlineDetailsWhole) {
+        this.deadlineDetailsWhole = deadlineDetailsWhole;
     }
 
     /**
      * Executes the Deadline command which creates a Deadline Task.
      *
-     * <p></p>Taking the TaskList and Storage object of the main Duke class as arguments, this command will create a
-     * new Deadline Task which will then be added to the TaskList and Storage objects. The UI will also be used to
-     * print a newTask message into the console.
-     *
      * @param taskList The TaskList from the main Duke object.
      * @param storage The Storage from the main Duke object.
+	 * @return a newTask message into the console.
      */
     public String execute(TaskList taskList, Storage storage) {
-        Task deadline = new Deadline(deadlineName, date);
-        taskList.addTaskList(deadline);
-        storage.writeToFile(taskList.getTaskList());
-        return UI.newTask(taskList.getTaskList());
+		try {
+			try {
+				String[] deadlineDetails = deadlineDetailsWhole.split(" /by ");
+				deadlineName = deadlineDetails[0].substring(9);
+				LocalDate date;
+				
+				if (deadlineDetails[1].equals("today")){
+					date = java.time.LocalDate.now();
+				} else {
+					date = LocalDate.parse(deadlineDetails[1]);
+				}
+				dateFormatted = date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+				Task deadline = new Deadline(deadlineName, dateFormatted);
+				taskList.addTaskList(deadline);
+				storage.writeToFile(taskList.getTaskList());
+				return UI.newTask(taskList.getTaskList());
+			} catch (ArrayIndexOutOfBoundsException | StringIndexOutOfBoundsException e){
+				if ( deadlineName == null || deadlineName.trim().length() == 0){
+					throw new Exception(Exception.Code.EMPTY_DEADLINE_DESCRIPTION);
+				} else {
+					throw new Exception(Exception.Code.MISSING_DEADLINE_DATETIME);
+				}
+			} catch (DateTimeParseException e){
+				throw new Exception(Exception.Code.WRONG_DEADLINE_FORMAT);
+			}
+		} catch (Exception e){
+			return e.errorDescription();
+		}
     }
 }
