bf070ef00791f95ec72c411f8e5bacba5f73fddd
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-17 22:29:49 +0800

    added Printing of Overdue tasks

--- src/main/java/DukeCommands/ListCommand.java
+++ src/main/java/DukeCommands/ListCommand.java
@@ -1,9 +1,12 @@
-package DukeCommands;
+package newDuke.DukeCommands;
 
-import main.Storage;
-import main.TaskList;
-import DukeTasks.*;
+import newDuke.main.Storage;
+import newDuke.main.TaskList;
+import newDuke.DukeTasks.*;
 import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * A Command to print a list of all the Tasks in the TaskList.
@@ -14,6 +17,17 @@ public class ListCommand implements Command {
     public ListCommand() {
 
     }
+	
+	public boolean isTaskOverdue(Task t){
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy");
+        String oldDateFormatted = t.getDate();
+		LocalDate taskDeadlineDate = LocalDate.parse(oldDateFormatted, formatter);
+		if ( taskDeadlineDate.isBefore(java.time.LocalDate.now())){
+			return true;
+		} else {
+			return false;
+		}
+	}
     /**
      * Prints into the console all the Tasks in the TaskList.
      *
@@ -22,10 +36,19 @@ public class ListCommand implements Command {
      */
 
     public String execute(TaskList taskList, Storage storage) {
+		boolean haveOverdueTasks = false;
 		System.out.println("Here are the tasks in your list:");
 		for (int a=1; a<taskList.getSize()+1;a++){
-		System.out.println("	"+a+". "+taskList.getTask(a-1) );
+			if (isTaskOverdue(taskList.getTask(a-1))){
+				System.out.println("	"+a+". "+taskList.getTask(a-1)+" !OVERDUE!" );
+				haveOverdueTasks = true;
+			} else {
+				System.out.println("	"+a+". "+taskList.getTask(a-1) );
+			}
+		}
+		if (haveOverdueTasks){
+			System.out.println("Please type 'snooze' followed by the Task Number, to postpone overdue task(s) by a week");
 		}
         return "\n";
-    }
+	}
 }
