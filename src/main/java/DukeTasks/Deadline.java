b1770f194b9bd044edf9bf6b89fcce9a70a54697
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-17 22:42:57 +0800

    Added javaDocs

--- src/main/java/DukeTasks/Deadline.java
+++ src/main/java/DukeTasks/Deadline.java
@@ -1,10 +1,16 @@
 package newDuke.DukeTasks; 
-import java.time.LocalDate;
 
-// Deadlines: tasks that need to be done before a specific date/time e.g., submit report by 11/10/2019 5pm
+/**
+ * A type of Task that includes a date attribute for the Task to be completed by. Also includes two SimpleDateFormatter.
+ */
 public class Deadline extends Task {
     protected String by;
-	
+    /**
+     * Constructor for a Deadline Task.
+     *
+     * @param description A String which represents the name of the Task.
+     * @param by A String which represents the date and time of the Task.
+     */
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
