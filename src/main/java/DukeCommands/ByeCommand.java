40f36292a7b65e74f5cdb827386ce4474044ed7d
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-17 22:22:12 +0800

    Update ByeCommand.java

--- src/main/java/DukeCommands/ByeCommand.java
+++ src/main/java/DukeCommands/ByeCommand.java
@@ -1,10 +1,10 @@
-package DukeCommands;
+package newDuke.DukeCommands;
 
-import main.Storage;
-import main.TaskList;
-import main.UI;
+import newDuke.main.Storage;
+import newDuke.main.TaskList;
+import newDuke.main.UI;
 /**
- * A Command to handle "bye" inputs resulting in a termination of the Duke program.
+ * A Command to handle "bye" input resulting in a termination of the Duke program.
  */
 
 public class ByeCommand implements Command {
