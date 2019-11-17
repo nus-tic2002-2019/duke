0a2a756ba7fb90675e6139c6289cb5fea04fa0e4
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-17 22:44:27 +0800

    Added javaDocs

--- src/main/java/DukeTasks/Event.java
+++ src/main/java/DukeTasks/Event.java
@@ -1,15 +1,28 @@
-package DukeTasks; 
-// Events: tasks that start at a specific time and ends at a specific time e.g., team project meeting on 2/10/2019 2-4pm
+package newDuke.DukeTasks; 
+/**
+ * A type of Task that includes a date attribute for the date in which the Task takes place.
+ */
 public class Event extends Task {
+	
     protected String at;
-
+	/**
+     * Constructor for an Event Task.
+     *
+     * @param description A String representing the name of the Task.
+     * @param at A String which represents the venue of the Task.
+     */
     public Event(String description, String at) {
         super(description);
         this.at = at;
+		this.setSymbol("E");
     }
 
     @Override
     public String toString() {
-        return "[E]" + this.getStatusIcon() +" "+ super.toString() + " (at: " + at + ")";
+        return "["+this.getSymbol()+"]" + this.getStatusIcon() +" "+ super.toString() + " (at: " + at + ")";
+    }
+	@Override
+	public String toSave() {
+        return this.getSymbol() + " | " + this.getStatus() + " | " + super.toString() + " | " + at;
     }
 }
