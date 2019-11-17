b1e7cbfaaecec114c3ec23c126aafafbe88d233e
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-17 23:09:29 +0800

    added Snooze

--- src/main/java/main/Parser.java
+++ src/main/java/main/Parser.java
@@ -1,55 +1,82 @@
-package main;
+package newDuke.main;
 
-import DukeCommands.*;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
+import newDuke.DukeCommands.*;
+import newDuke.DukeExceptions.Exception;
 
 public class Parser {
+	
+	public Parser() {
+    	}
 
-    public Parser() {
-
-    }
+    	private void commandHasInput(String[] nextCommand) {
+		assert nextCommand.length > 1 : "Command does not have an Input.";
+    	}
+	private void commandHasTaskNumber(String nextCommand) {
+		Object obj = Integer.parseInt(nextCommand);
+		assert obj instanceof Integer: "Command does not have a Task.";
+    	}
+	
+	/**
+	* Parses the input and returns a corresponding Command
+	*
+	* <p></p>When a String (commandLine) is supplied, this method will split commandLine into multiple words (via
+	* String.split(" "). This will result in a String array containing individual words. The first word is then used
+	* to determine which Command is to be returned.
+	*
+	* @param commandLine Entire user input in String.
+	* @return Returns a corresponding Command object.
+	*/
 	public Command parse(String commandLine) {
 
-        String[] nextCommand = commandLine.split(" ");
+	String[] nextCommand = commandLine.split(" ");
+		
+	switch (nextCommand[0]) {
+	case "hello":
+		return new StartCommand();
 
-        switch (nextCommand[0]) {
-		case "hello":
-            return new StartCommand();
-			
-		case "bye":
-            return new ByeCommand();
-			
-		case "list":
-            return new ListCommand();
-			
-		case "todo":
-            String todoName = "";
-            for (int i = 1; i < nextCommand.length; i++) {
-                todoName += nextCommand[i] + " ";
-            }
-            return new TodoCommand(todoName);
-			
-        case "done":
-            return new DoneCommand(Integer.valueOf(nextCommand[1]) - 1);
-			
-        case "event":
-            String[] eventDetails = commandLine.split(" /at ");
-            return new EventCommand(eventDetails[0].substring(6), eventDetails[1]);
+	case "bye":
+		return new ByeCommand();
 
-        case "deadline":
-            String[] deadlineDetails = commandLine.split(" /by ");
-	    LocalDate d1 = LocalDate.parse(deadlineDetails[1]);
-            return new DeadlineCommand(deadlineDetails[0].substring(9), d1.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
+	case "list":
+		return new ListCommand();
+
+	case "todo":
+		String todoName = "";
+		for (int i = 1; i < nextCommand.length; i++) {
+			todoName += nextCommand[i] + " ";
+		}
+		return new TodoCommand(todoName);
+
+	case "done":
+		return new DoneCommand(nextCommand);
+
+	case "event":
+		return new EventCommand(commandLine);
+
+	case "deadline":
+		return new DeadlineCommand(commandLine);
 			
 	case "delete":
-            int index = Integer.parseInt(nextCommand[1]);
-            return new DeleteCommand(index);
+		commandHasInput(nextCommand);
+		commandHasTaskNumber(nextCommand[1]);
+		int index = Integer.parseInt(nextCommand[1]);
+		return new DeleteCommand(index);
+
+	case "find":
+		commandHasInput(nextCommand);
+		commandHasTaskNumber(nextCommand[1]);
+		return new FindCommand(nextCommand[1]);
+
+	case "snooze":
+		commandHasInput(nextCommand);
+		commandHasTaskNumber(nextCommand[1]);
+		index = Integer.parseInt(nextCommand[1]);
+		return new SnoozeCommand(index);
 
 	default:
-            break;
-        }
-        return new UnknownCommand();
-    }
+		break;
+	}
+		return new UnknownCommand();
+	}
+
 }
