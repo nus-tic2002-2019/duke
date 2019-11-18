9ddf6fef65c07ab64b65262cc4736199150ec675
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-18 16:15:38 +0800

    added getDate and setDate

--- src/main/java/DukeTasks/Task.java
+++ src/main/java/DukeTasks/Task.java
@@ -7,82 +7,82 @@ package newDuke.DukeTasks;
  * to be implemented by any classes that extend the Task abstract class.
  */
 public abstract class Task {
-	protected String description;
-	protected boolean isDone;
+    protected String description;
+    protected boolean isDone;
 	protected String symbol;
 
-	public Task(String description) {
-	this.description = description;
-	this.isDone = false;
-	}
-	
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
 	/**
 	 * Returns the description of the Task.
 	 *
 	 * @return String which is the description of the Task.
 	 */
 	public String description() {
-	return description;
-	}
-	
+        return description;
+    }
 	/**
 	 * Returns the status icon of the Task.
 	 *
-	* @return String which is the status icon of the Task.
-	*/
-	public String getStatusIcon() {
-	return (isDone ? "[V]" : "[X]"); //return tick or X symbols
-	}
+     * @return String which is the status icon of the Task.
+     */
+    public String getStatusIcon() {
+        return (isDone ? "[V]" : "[X]"); //return tick or X symbols
+    }
 	/**
-	* Returns the done status (true/false) of the Task.
-	*
-	* @return String which is the done status (true/false) of the Task.
-	*/
+     * Returns the done status (true/false) of the Task.
+     *
+     * @return String which is the done status (true/false) of the Task.
+     */
 	public int getStatus() {
-	return (isDone ? 1 : 0); //return 1 or 0 symbols
-	}
+        return (isDone ? 1 : 0); //return 1 or 0 symbols
+    }
 	/**
-	* Indicates that this Task has already been completed.
-	*/
+     * Indicates that this Task has already been completed.
+     */
 	public void setStatusIconTrue() {
-	this.isDone = true;
-	}
+        this.isDone = true;
+    }
 	/**
-	* Indicates that this Task has not been completed. 
+     * Indicates that this Task has not been completed. 
 	 * For future use cases or regression that might require status to be set to false.
-	*/
+     */
 	public void setStatusIconFalse() {
-	this.isDone = false;
-	}
+        this.isDone = false;
+    }
 	/**
-	* Returns the Task type (T/E/D) of the Task.
-	*
-	* @return String which is the Task type (T/E/D) of the Task.
-	*/
+     * Returns the Task type (T/E/D) of the Task.
+     *
+     * @return String which is the Task type (T/E/D) of the Task.
+     */
 	public String getSymbol() {
-	return symbol;
-	}
+        return symbol;
+    }
 	/**
-	* Sets the Task type (T/E/D).
-	*/
+     * Sets the Task type (T/E/D).
+     */
 	public void setSymbol(String symbol) {
-	this.symbol = symbol;
-	}
+        this.symbol = symbol;
+    }
 	/**
-	* Returns a String description of the Task
-	*
-	* @return A String description of the Task.
-	*/
+     * Returns a String description of the Task
+     *
+     * @return A String description of the Task.
+     */
 	@Override
-	public String toString() {
-	return description;
-	}
+    public String toString() {
+        return description;
+    }
 	/**
-	* Returns a String representing the Task in a format to be saved into the hard disk's storage file.
-	*
-	* <p></p>To be implemented by each individual Task class.
-	*
-	* @return A String representation of this Task formatted for the storage file.
-	*/
+     * Returns a String representing the Task in a format to be saved into the hard disk's storage file.
+     *
+     * <p></p>To be implemented by each individual Task class.
+     *
+     * @return A String representation of this Task formatted for the storage file.
+     */
 	public abstract String toSave();
+	public abstract String getDate();
+	public abstract void setDate(String newDate);
 }
