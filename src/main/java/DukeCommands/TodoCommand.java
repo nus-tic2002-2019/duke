0594c4520eb7a93b5c00f96fe34ac0f78cd66cd0
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2019-11-18 15:38:13 +0800

    fixed a bug
    previously: no task description exception bypassed

--- src/main/java/DukeCommands/TodoCommand.java
+++ src/main/java/DukeCommands/TodoCommand.java
@@ -13,36 +13,33 @@ import newDuke.DukeExceptions.Exception;
 
 public class TodoCommand implements Command {
 
-    private String todoName;
-    public TodoCommand(String todoName) {
-        this.todoName = todoName;
-    }
+	private String todoName;
+	public TodoCommand(String todoName) {
+		this.todoName = todoName;
+	}
 
-    /**
-     * Executes the Todo command which creates a Todo Task.
-     *
-     * <p></p>Taking the TaskList and Storage object of the main Duke class as arguments, this command will create a new
-     * Deadline Task which will then be added to the TaskList and Storage objects. The UI will also be used to print a newTask message into the console.
-     *
-     * @param taskList The TaskList from the main Duke object.
-     * @param storage The Storage from the main Duke object.
-     */
+	/**
+	 * Executes the Todo command which creates a Todo Task.
+	 *
+	 * <p></p>Taking the TaskList and Storage object of the main Duke class as arguments, this command will create a new
+	 * Deadline Task which will then be added to the TaskList and Storage objects. The UI will also be used to print a newTask message into the console.
+	 *
+	 * @param taskList The TaskList from the main Duke object.
+	 * @param storage The Storage from the main Duke object.
+	 */
 
-    public String execute(TaskList taskList, Storage storage) {
+	public String execute(TaskList taskList, Storage storage) {
 		try {
-			try {
-				ArrayList<Task> list = taskList.getTaskList();
-				Task todo = new Todo(todoName);
-				taskList.addTaskList(todo);
-				storage.writeToFile(list);
-				return UI.newTask(list);
-			} catch (ArrayIndexOutOfBoundsException | StringIndexOutOfBoundsException e){
-				if ( todoName==null || todoName.trim().length() == 0){
-					throw new Exception(Exception.Code.EMPTY_TODO_DESCRIPTION);
-				}
-			} catch (Exception e){
-				return e.errorDescription();
+			if ( todoName==null || todoName.trim().length() == 0){
+				throw new Exception(Exception.Code.EMPTY_TODO_DESCRIPTION);
 			}
+			ArrayList<Task> list = taskList.getTaskList();
+			Task todo = new Todo(todoName);
+			taskList.addTaskList(todo);
+			storage.writeToFile(list);
+			return UI.newTask(list);
+		} catch (Exception e){
+			return e.errorDescription();
 		}
 	}
 }
